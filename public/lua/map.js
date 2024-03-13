d980c810e04d238ac0e42b7eec38804eb49de6e5
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-03-12 19:07:06 -0700

    feat(lua): map

--- public/lua/map.js
+++ public/lua/map.js
@@ -1,84 +1,98 @@
-function get_cell() {
-	try {
-		return JSON.parse(window.swf.Cell());
-	} catch {
-		return null;
+class map {
+	static get_cell() {
+		try {
+			return JSON.parse(window.swf.getGameObject('world.strFrame'));
+		} catch {
+			return undefined;
+		}
 	}
+
+	static get_pad() {
+		try {
+			return JSON.parse(window.swf.getGameObject('world.strPad'));
+		} catch {
+			return undefined;
+		}
+	}
+
+	static get_name() {
+		try {
+			return JSON.parse(window.swf.getGameObject('world.strMapName'));
+		} catch {
+			return undefined;
+		}
+	}
+
+	static get_id() {
+		try {
+			return JSON.parse(window.swf.getGameObject('world.curRoom'));
+		} catch {
+			return undefined;
+		}
+	}
+
+	static get_player_count() {
+		try {
+			return JSON.parse(window.swf.getGameObject('world.areaUsers.length'));
+		} catch {
+			return undefined;
+		}
+	}
+
+	static get_players() {
+		try {
+			return JSON.parse(window.swf.getGameObject('world.areaUsers'));
+		} catch {
+			return undefined;
+		}
+	}
+
+	static is_loaded() {
+		try {
+			const inProgress = JSON.parse(window.swf.getGameObject('world.mapLoadInProgress'));
+			const isNull = JSON.parse(window.swf.isNull('mcConnDetail.stage'));
+			return !inProgress && isNull;
+		} catch {
+			return undefined;
+		}
+	}
+
+	static get_cells() {
+		try {
+			const ret = JSON.parse(window.swf.getGameObject('world.map.currentScene.labels'));
+			return ret.map((cell) => cell.name);
+		} catch {
+			return undefined;
+		}
+	}
+
+	static jump(cell, pad, autoCorrect = true, clientOnly = false) {
+		try {
+			window.swf.jumpCorrectRoom(cell, pad, autoCorrect, clientOnly);
+		} catch {}
+	}
+
+	static join(map, cell = 'Enter', pad = 'Spawn') {
+		try {
+			window.swf.callGameFunction('world.gotoTown', map, cell, pad);
+		} catch {}
+	}
+
+	static reload() {
+		try {
+			window.swf.callGameFunction('world.reloadCurrentMap');
+		} catch {}
+	}
+
+	static get_item(id) {
+		try {
+			window.swf.callGameFunction('world.getMapItem', id);
+		} catch {
+			return undefined;
+		}
+	}
+
+	// TODO: get map items by using FFDEC
 }
 
-function get_pad() {
-	try {
-		return JSON.parse(window.swf.Pad());
-	} catch {
-		return null;
-	}
-}
-
-function get_cells() {
-	try {
-		return JSON.parse(window.swf.GetCells());
-	} catch {
-		return [];
-	}
-}
-
-function get_room_id() {
-	try {
-		return JSON.parse(window.swf.RoomId());
-	} catch {
-		return null;
-	}
-}
-
-function get_room_number() {
-	try {
-		return JSON.parse(window.swf.RoomNumber());
-	} catch {
-		return -1;
-	}
-}
-
-function get_players() {
-	try {
-		return JSON.parse(window.swf.Players());
-	} catch {
-		return [];
-	}
-}
-
-function get_player(param1) {
-	try {
-		return JSON.parse(window.swf.GetPlayer(param1));
-	} catch {
-		return null;
-	}
-}
-
-function is_player_in_cell(param1, param2) {
-	try {
-		return JSON.parse(window.swf.CheckPlayerCell(param1, param2));
-	} catch {
-		return false;
-	}
-}
-
-function get_room_name() {
-	try {
-		const res = JSON.parse(swf.getGameObject('world.strAreaName'));
-		return res.split('-')[0];
-	} catch {
-		return null;
-	}
-}
-
-module.exports = {
-	get_cell,
-	get_pad,
-	get_cells,
-	get_room_id,
-	get_room_name,
-	get_room_number,
-	get_players,
-	get_player,
-	is_player_in_cell,
-};
+module.exports = map;
