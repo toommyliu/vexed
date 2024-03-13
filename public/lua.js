9d3eae74a295fcc4bfaf161cf69bbed4d15de253
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-03-12 20:35:17 -0700

    refactor(lua): load runtime after fengari script

--- public/lua.js
+++ public/lua.js
@@ -1,38 +1,25 @@
 let script = document.createElement('script');
 script.src = 'https://cdn.jsdelivr.net/npm/fengari-web@0.1.4/dist/fengari-web.min.js';
+script.defer = true;
 
-document.body.appendChild(script);
+script.onload = () => {
+	const fwddec = require('./lua/fwddec');
+	const { fengari } = window;
 
-script = document.createElement('script');
-script.textContent = `
-	window.lua_ready = false;
-	window.load_lua_runtime = function () {
-		if (window.lua_ready) {
-			return;
-		}
+	for (const tbl of Object.keys(fwddec)) {
+		window[tbl] = fwddec[tbl];
 
-		const fwddec = require('./lua/fwddec');
-		const { fengari } = window;
-
-		for (const tbl of Object.keys(fwddec)) {
-			window[tbl] = fwddec[tbl];
-
-			// push the table
-			fengari.interop.push(fengari.L, window[tbl]);
-			// set the variable
-			fengari.lua.lua_setglobal(fengari.L, fengari.to_luastring(tbl));
-		}
-
-		window.lua_ready = true;
+		// push the table
+		fengari.interop.push(fengari.L, window[tbl]);
+		// set the variable
+		fengari.lua.lua_setglobal(fengari.L, fengari.to_luastring(tbl));
 	}
 
-	function executeLua(code) {
-		if (!window.lua_ready) {
-			window.load_lua_runtime();
-		}
-
+	window.executeLua = function (code) {
 		return fengari.load(code)();
-	}
-`;
+	};
+
+	console.log('Loaded lua runtime');
+};
 
 document.body.appendChild(script);
