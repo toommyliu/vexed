83b46b0d037506d1bb815a28d80dca7c39e6934d
Tommy Liu <86510873+tommyliu@users.noreply.github.com>
2024-04-04 22:21:44 -0700

    fix: remove dead code

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -12,17 +12,11 @@ export const metadata: Metadata = {
 	},
 };
 
-interface RootLayoutProps {
-	children: React.ReactNode;
-}
-
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<>
-			<html lang="en" suppressHydrationWarning>
-				<head />
-				<body className={inter.className}>{children}</body>
-			</html>
-		</>
+		<html lang="en" suppressHydrationWarning>
+			<head />
+			<body className={inter.className}>{children}</body>
+		</html>
 	);
 }
