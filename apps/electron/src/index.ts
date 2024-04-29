cccbbb60643fe16231888769681a959d0be3f284
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-04-28 17:45:18 -0700

    chore: cherry pick from #11

--- apps/electron/src/index.ts
+++ apps/electron/src/index.ts
@@ -1,31 +1,45 @@
+import { BrowserWindow, app, session } from 'electron';
+import fs from 'fs';
 import { join } from 'path';
-import { app, BrowserWindow } from 'electron';
 
-{
+const userAgent =
+	'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ArtixGameLauncher/2.0.9 Chrome/80.0.3987.163 Electron/8.5.5 Safari/537.36';
+
+const BRAND = 'Vexed';
+
+function registerFlashPlugin() {
 	const flashTrust = require('nw-flash-trust');
 	// TODO: add checks for app.isPackaged
 	app.commandLine.appendSwitch('ppapi-flash-path', join(__dirname, '../vendor/PepperFlashPlayer.plugin'));
 
 	const flashPath = join(app.getPath('userData'), 'Pepper Data', 'Shockwave Flash', 'WritableRoot');
 
-	const trustManager = flashTrust.initSync('Vexed', flashPath);
+	const trustManager = flashTrust.initSync(BRAND, flashPath);
 	trustManager.empty();
-	trustManager.add(join(__dirname, '../public/'));
+	trustManager.add(join(__dirname, '../grimoire.swf'));
 }
 
+registerFlashPlugin();
+
 app.once('ready', async () => {
+	await fs.promises.mkdir(join(app.getPath('documents'), BRAND, 'Scripts'), { recursive: true });
+
 	const window = new BrowserWindow({
-		width: 960,
-		height: 550,
+		width: 1024,
+		height: 576,
 		webPreferences: {
-			contextIsolation: false,
-			enableRemoteModule: true,
-			nodeIntegration: true,
 			plugins: true,
 		},
+		title: '',
 	});
 
-	await window.loadFile(join(__dirname, '../public/index.html'));
+	window.webContents.setUserAgent(userAgent);
+	session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+		details.requestHeaders['User-Agent'] = userAgent;
+		callback({ requestHeaders: details.requestHeaders, cancel: false });
+	});
+
+	await window.loadFile(join(__dirname, '../index.html'));
 
 	window.webContents.openDevTools({ mode: 'detach' });
 });
