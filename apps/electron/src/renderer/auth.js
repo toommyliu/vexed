4ee5f6c9f820bdd5d44f285cd0cca3a5cf0c9c28
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-05-03 15:30:56 -0700

    chore: jsdoc

--- apps/electron/src/renderer/auth.js
+++ apps/electron/src/renderer/auth.js
@@ -1,18 +1,32 @@
 class Auth {
+	/**
+	 * @returns {string|null}
+	 */
 	static get username() {
 		if (!Auth.loggedIn) return null;
 		return Flash.call(window.swf.GetUsername);
 	}
 
+	/**
+	 * @returns {string|null}
+	 */
 	static get password() {
 		if (!Auth.loggedIn) return null;
 		return Flash.call(window.swf.GetPassword);
 	}
 
+	/**
+	 * @returns {boolean}
+	 */
 	static get loggedIn() {
 		return Flash.call(window.swf.IsLoggedIn);
 	}
 
+	/**
+	 * @param {string} [username]
+	 * @param {string} [password]
+	 * @returns {void}
+	 */
 	static login(username, password) {
 		if (username && password) {
 			if (typeof username !== 'string') throw new Error('username must be a string');
@@ -24,18 +38,31 @@ class Auth {
 		}
 	}
 
+	/**
+	 * @returns {void}
+	 */
 	static logout() {
 		Flash.call(window.swf.Logout);
 	}
 
+	/**
+	 * @returns {Server[]}
+	 */
 	static get servers() {
 		return Flash.call(window.swf.getGameObject, 'serialCmd.servers');
 	}
 
+	/**
+	 * @returns {boolean}
+	 */
 	static resetServers() {
 		return Flash.call(window.swf.ResetServers);
 	}
 
+	/**
+	 * @param {string} name
+	 * @returns {void}
+	 */
 	static connect(name) {
 		if (typeof name !== 'string') throw new Error('server name must be a string');
 		if (!Auth.servers.find((s) => s.sName.toLowerCase() === name.toLowerCase()))
@@ -44,3 +71,17 @@ class Auth {
 		Flash.call(window.swf.Connect, name);
 	}
 }
+
+/**
+ * @typedef {Object} Server
+ * @property {number} iMax Maximum number of players
+ * @property {number} iPort The port number the server is on
+ * @property {string} sLang The language of the server (en/pt)
+ * @property {string} sName The name of the server
+ * @property {string} sIP The IP address of the server
+ * @property {number} iCount The number of current players
+ * @property {number} bUpg Whether the server is an upgrade-only server
+ * @property {number} iLevel
+ * @property {number} bOnline Whether the server is online (0, 1)
+ * @property {number} iChat The chat-level restriction of the server (0=canned, 2=free)
+ */
