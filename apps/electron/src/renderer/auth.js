a44960792a3acf618764a0c57104a88c38c5fa09
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-05-03 14:39:47 -0700

    feat: add runtime checks for auth

--- apps/electron/src/renderer/auth.js
+++ apps/electron/src/renderer/auth.js
@@ -15,8 +15,11 @@ class Auth {
 
 	static login(username, password) {
 		if (username && password) {
+			if (typeof username !== 'string') throw new Error('username must be a string');
+			if (typeof password !== 'string') throw new Error('password must be a string');
 			Flash.call(window.swf.FixLogin, username, password);
 		} else {
+			if (!Auth.username && !Auth.password) throw new Error('login manually once before calling Auth#login');
 			Flash.call(window.swf.Login);
 		}
 	}
@@ -30,10 +33,14 @@ class Auth {
 	}
 
 	static resetServers() {
-		Flash.call(window.swf.ResetServers);
+		return Flash.call(window.swf.ResetServers);
 	}
 
 	static connect(name) {
+		if (typeof name !== 'string') throw new Error('server name must be a string');
+		if (!Auth.servers.find((s) => s.sName.toLowerCase() === name.toLowerCase()))
+			throw new Error('server name not found in list');
+
 		Flash.call(window.swf.Connect, name);
 	}
 }
