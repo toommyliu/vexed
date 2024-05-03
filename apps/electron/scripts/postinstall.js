130d72c85877994a93937d9807e1e846950429f9
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-05-02 20:14:50 -0700

    refactor: cleanup postinstall script

---
+++ apps/electron/scripts/postinstall.js
@@ -0,0 +1,39 @@
+// https://github.com/electron/electron/blob/11-x-y/npm/install.js
+
+console.log('Downloading Electron 11.5.0 Intel binary');
+
+const fs = require('fs');
+const path = require('path');
+const extract = require('extract-zip');
+const { downloadArtifact } = require('@electron/get');
+
+(async () => {
+	const zipPath = await downloadArtifact({
+		version: '11.5.0',
+		artifactName: 'electron',
+		force: process.env.force_no_cache === 'true',
+		cacheRoot: process.env.electron_config_cache,
+		platform: 'darwin',
+		arch: 'x64',
+	})
+		.then((res) => res)
+		.catch((err) => {
+			console.error('An error occured while downloading the Intel binary:', err.stack);
+			process.exit(1);
+		});
+
+	if (zipPath) {
+		const nodeModules = path.join(__dirname, '..', 'node_modules');
+		const electron = path.join(nodeModules, 'electron');
+		const electronDist = path.join(electron, 'dist');
+
+		await fs.promises.rm(electronDist, { recursive: true }).catch(() => {
+			console.error('Failed to delete old Electron files');
+			process.exit(1);
+		});
+		await extract(zipPath, { dir: electronDist }).catch(() => {
+			console.error('Failed to extract new Electron files');
+			process.exit(1);
+		});
+	}
+})();
