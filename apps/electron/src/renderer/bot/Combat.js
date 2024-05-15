080b7ab978928ecc2d1718afac2d17d8ca25e716
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-05-15 11:39:17 -0700

    feat: combat#killForItem, improve combat#kill

--- apps/electron/src/renderer/bot/Combat.js
+++ apps/electron/src/renderer/bot/Combat.js
@@ -1,3 +1,5 @@
+const { setIntervalAsync, clearIntervalAsync } = require('set-interval-async');
+
 class Combat {
 	/**
 	 * @param {Bot} instance
@@ -10,17 +12,25 @@ class Combat {
 		 * @type {number[]}
 		 */
 		this.skillSet = [1, 2, 3, 4];
+
 		/**
 		 * The index of the skill set to use.
 		 * @type {number}
 		 * @private
 		 */
 		this.skillSetIdx = 0;
+
 		/**
 		 * The delay between skills.
 		 * @type {number}
 		 */
 		this.skillDelay = 150;
+
+		/**
+		 * Internal interval ID.
+		 * @type {number}
+		 */
+		this.intervalId = -1;
 	}
 
 	/**
@@ -91,19 +101,70 @@ class Combat {
 
 		this.attack(name);
 
-		while (isMonAlive() && this.instance.isRunning) {
-			if (this.hasTarget) {
-				this.useSkill(this.skillSet[this.skillSetIdx], false);
-				this.skillSetIdx++;
+		this.intervalId = setIntervalAsync(async () => {
+			if (isMonAlive() && this.instance.isRunning) {
+				if (this.hasTarget) {
+					this.useSkill(this.skillSet[this.skillSetIdx], false);
+					this.skillSetIdx++;
 
-				// cycle skills
-				if (this.skillSetIdx >= this.skillSet.length) {
-					this.skillSetIdx = 0;
+					// cycle skills
+					if (this.skillSetIdx >= this.skillSet.length) {
+						this.skillSetIdx = 0;
+					}
+				}
+				await this.instance.sleep(this.skillDelay);
+			} else {
+				if (!isMonAlive()) {
+					(async () => {
+						await clearIntervalAsync(this.intervalId);
+						this.intervalId = -1;
+					})();
+				}
+				await this.instance.sleep(1000);
+				if (this.instance.player.isAfk) {
+					(async () => {
+						await clearIntervalAsync(this.intervalId);
+						this.intervalId = -1;
+					})();
 				}
 			}
-			await this.instance.sleep(this.skillDelay);
+		}, 0);
+
+		while (isMonAlive() && this.intervalId !== -1) {
+			await this.instance.sleep(1000);
 		}
 	}
 
-	// async killForItem(name, item, quantity = '*') {}
+	async killForItem(name, item, quantity = '*', temp = false) {
+		const getItem = () => {
+			if (temp) {
+				return this.instance.tempInventory.items.find((i) => i.name.toLowerCase() === item.toLowerCase());
+			}
+			return this.instance.inventory.items.find((i) => i.name.toLowerCase() === item.toLowerCase());
+		};
+		const getQuantity = () => getItem()?.quantity ?? 0;
+		const checkRet = () => (getQuantity() > 1 && quantity === '*') || getQuantity() >= Number.parseInt(quantity);
+
+		if (checkRet()) return;
+
+		await this.kill(name);
+		await this.instance.sleep(1000);
+		const i = Object.values(this.instance.world.dropStack.drops).find(
+			(d) => d[0].sName.toLowerCase() === item.toLowerCase(),
+		);
+
+		// Item is in dropstack
+		if (i) {
+			await this.instance.world.dropStack.collect(i[0].ItemID);
+
+			if (checkRet()) return;
+
+			await this.killForItem(name, item, quantity, temp);
+		} else {
+			// Item is in inventory
+			if (checkRet()) return;
+
+			await this.killForItem(name, item, quantity, temp);
+		}
+	}
 }
