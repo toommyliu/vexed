8fd2501e0739e17377a8bdac2b8ee0e18baadbb4
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-05-29 14:16:36 -0700

    chore: respect linter

--- apps/electron/scripts/gen-jsdoc.js
+++ apps/electron/scripts/gen-jsdoc.js
@@ -1,35 +1,45 @@
-const jsdoc2md = require('jsdoc-to-markdown');
-const fs = require('fs');
-const path = require('path');
-const util = require('util');
+const jsdoc2md = require("jsdoc-to-markdown");
+const fs = require("node:fs");
+const path = require("node:path");
 
-const inputDir = path.join(__dirname, '../src/renderer/jsapi');
-const outputDir = path.join(__dirname, '../../docs/api');
+const inputDir = path.join(__dirname, "../src/renderer/jsapi");
+const outputDir = path.join(__dirname, "../../docs/api");
 
 async function gen() {
-    const inputs = await fs.promises.readdir(inputDir, { recursive: true })
-    const _inputs = inputs.map((i) => path.join(inputDir, i));
-    const templateData = await jsdoc2md.getTemplateData({ files: _inputs });
+	const inputs = await fs.promises.readdir(inputDir, { recursive: true });
+	const _inputs = inputs.map((i) => path.join(inputDir, i));
+	const templateData = await jsdoc2md.getTemplateData({ files: _inputs });
 
-    const classNames = templateData.reduce((classNames, identifier) => {
-        if (identifier.kind === 'class') classNames.push(identifier.name);
-        return classNames;
-    }, []);
+	const classNames = templateData.reduce((classNames, identifier) => {
+		if (identifier.kind === "class") classNames.push(identifier.name);
+		return classNames;
+	}, []);
 
-    const obj = [];
-    for (const className of classNames) {
-        const template = `{{#class name="${className}"}}{{>docs}}{{/class}}`;
-        console.log(`rendering ${className}`);
-        const output = await jsdoc2md.render({ data: templateData, template: template });
-        await fs.promises.writeFile(path.resolve(outputDir, `${className.toLowerCase()}.md`), output);
-        obj.push({ text: `"${className}"`, link: `"/api/${className.toLowerCase()}"` });
-    }
-    const out = obj.map(item => `{ text: ${item.text}, link: ${item.link} },`).join('\n');
-    console.log(out);
+	const obj = [];
+	for (const className of classNames) {
+		const template = `{{#class name="${className}"}}{{>docs}}{{/class}}`;
+		console.log(`rendering ${className}`);
+		const output = await jsdoc2md.render({
+			data: templateData,
+			template: template
+		});
+		await fs.promises.writeFile(
+			path.resolve(outputDir, `${className.toLowerCase()}.md`),
+			output
+		);
+		obj.push({
+			text: `"${className}"`,
+			link: `"/api/${className.toLowerCase()}"`
+		});
+	}
+	const out = obj
+		.map((item) => `{ text: ${item.text}, link: ${item.link} },`)
+		.join("\n");
+	console.log(out);
 }
 
 gen()
-    .then(() => console.log('done'))
-    .catch((e) => {
-        console.log('failed to gen jsdoc:', e);
-    });
+	.then(() => console.log("done"))
+	.catch((e) => {
+		console.log("failed to gen jsdoc:", e);
+	});
--- apps/electron/scripts/postinstall.js
+++ apps/electron/scripts/postinstall.js
@@ -1,36 +1,41 @@
 // https://github.com/electron/electron/blob/11-x-y/npm/install.js
 
-console.log('Downloading Electron 11.5.0 Intel binary');
+console.log("Downloading Electron 11.5.0 Intel binary");
 
-const fs = require('fs');
-const path = require('path');
-const extract = require('extract-zip');
-const { downloadArtifact } = require('@electron/get');
-const { promisify } = require('util');
+const fs = require("node:fs");
+const path = require("node:path");
+const extract = require("node:extract-zip");
+const { downloadArtifact } = require("node:@electron/get");
+const { promisify } = require("node:util");
 
 (async () => {
-	const nodeModules = path.join(__dirname, '../../../node_modules');
-	const electron = path.join(nodeModules, 'electron');
-	const electronDist = path.join(electron, 'dist');
+	const nodeModules = path.join(__dirname, "../../../node_modules");
+	const electron = path.join(nodeModules, "electron");
+	const electronDist = path.join(electron, "dist");
 
-	const exists = await promisify(fs.exists)(path.join(electronDist, '_version.txt'));
+	const exists = await promisify(fs.exists)(
+		path.join(electronDist, "_version.txt")
+	);
 
 	if (exists) {
-		console.log('Electron 11.5.0 already exists, skipping download');
+		console.log("Electron 11.5.0 already exists, skipping download");
 		return;
 	}
 
 	const zipPath = await downloadArtifact({
-		version: '11.5.0',
-		artifactName: 'electron',
-		force: process.env.force_no_cache === 'true',
+		version: "11.5.0",
+		artifactName: "electron",
+		force: process.env.force_no_cache === "true",
 		cacheRoot: process.env.electron_config_cache,
-		platform: 'darwin',
-		arch: 'x64',
+		platform: "darwin",
+		arch: "x64"
 	})
 		.then((res) => res)
 		.catch((err) => {
-			console.error('An error occured while downloading the Intel binary:', err.stack);
+			console.error(
+				"An error occured while downloading the Intel binary:",
+				err.stack
+			);
 			process.exit(1);
 		});
 
@@ -40,7 +45,7 @@ const { promisify } = require('util');
 		try {
 			await fs.promises.rm(electronDist, { recursive: true });
 			await extract(zipPath, { dir: electronDist });
-			await writeFile(path.join(electronDist, '_version.txt'), '11.5.0');
+			await writeFile(path.join(electronDist, "_version.txt"), "11.5.0");
 		} catch (error) {
 			console.log(error);
 		}
