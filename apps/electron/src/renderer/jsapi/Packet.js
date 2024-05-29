f045c85f2b2a31bf580490da9ffb57a9d4bc2508
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-05-28 21:58:21 -0700

    docs: update

--- apps/electron/src/renderer/jsapi/Packet.js
+++ apps/electron/src/renderer/jsapi/Packet.js
@@ -12,27 +12,20 @@ class Packet {
 	/**
 	 * Sends the specified packet to the client (simulates a response as if the server sent the packet).
 	 * @param {string} packet
-	 * @param {("str"|"json"|"xml")} [type="str"]
+	 * @param {"str"|"json"|"xml"} [type="str"]
 	 * @returns {void}
 	 */
 	sendClient(packet, type = 'str') {
-		return this.bot.flash.call(window.swf.sendClientPacket, packet, type);
+		this.bot.flash.call(window.swf.sendClientPacket, packet, type);
 	}
 
 	/**
 	 * Sends the specified packet to the server.
 	 * @param {string} packet
-	 * @param {("String"|"Json")} [type="String"]
+	 * @param {"String"|"Json"} [type="String"]
 	 * @returns {void}
 	 */
 	sendServer(packet, type = 'String') {
-		return this.bot.flash.call(window.swf.callGameFunction, `sfc.send${type}`, packet);
+		this.bot.flash.call(window.swf.callGameFunction, `sfc.send${type}`, packet);
 	}
 }
-
-/**
- * @typedef {string} ClientPacketType
- * @property {string} str
- * @property {string} json
- * @property {string} xml
- */
