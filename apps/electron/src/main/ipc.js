e4c0f085db17ea2ebfcb901b65bf2efc30ee8754
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-06-05 19:45:08 -0700

    refactor: further improve packet logger

--- apps/electron/src/main/ipc.js
+++ apps/electron/src/main/ipc.js
@@ -1,6 +1,23 @@
-const { ipcMain: ipc, BrowserWindow } = require("electron");
+const { app, ipcMain: ipc, BrowserWindow, dialog } = require("electron");
 const { assignWindowID, createPacketsWindow, getPacketsWindow } = require("./util/createWindow");
 const { nanoid } = require("nanoid");
+const fs = require("fs-extra");
+const { join } = require("path");
+
+const ROOT = join(app.getPath("documents"), "Vexed");
+
+//#region packets
+ipc.on("packets:save", async function(_, packets)
+{
+	try
+	{
+		await fs.writeFile(join(ROOT, "packets.txt"), packets.join("\n"), { encoding: "utf-8 "});
+	} catch
+	{
+		dialog.showErrorBox("error", "failed to write packets to file.")
+	}
+});
+//#endregion
 
 ipc.handle("window:create:packets", async (_, id) => {
 	await createPacketsWindow(id);
