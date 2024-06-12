8f29f1b26dbe63706e90960a812083f49d4d820c
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-06-12 11:54:12 -0700

    feat: improve combat#kill logged out + player death detection
    - added wip combat#kill afk detection

--- apps/electron/src/renderer/game/jsapi/Combat.js
+++ apps/electron/src/renderer/game/jsapi/Combat.js
@@ -1,10 +1,15 @@
 var { setIntervalAsync, clearIntervalAsync } = require("set-interval-async/fixed");
+var { Mutex } = require("async-mutex");
 
-class Combat {
+class Combat
+{
 	#intervalID;
 	#skillSetIdx = 0;
+	#mutex = new Mutex();
+	#stop = false;
 
-	constructor(bot) {
+	constructor(bot)
+	{
 		/**
 		 * @type {Bot}
 		 */
@@ -28,8 +33,10 @@ class Combat {
 	 * @param {string} monsterResolvable The name or monMapID of the monster.
 	 * @returns {void}
 	 */
-	attack(monsterResolvable) {
-		if (["id'", "id.", "id:", "id-"].some((prefix) => monsterResolvable.startsWith(prefix))) {
+	attack(monsterResolvable)
+	{
+		if (["id'", "id.", "id:", "id-"].some((prefix) => monsterResolvable.startsWith(prefix)))
+		{
 			const monMapID = monsterResolvable.substring(3);
 			this.bot.flash.call(window.swf.AttackMonsterByMonMapId, monMapID);
 			return;
@@ -43,10 +50,12 @@ class Combat {
 	 * @param {boolean} [force=false] Whether to force the skill to be used (e.g on a random target).
 	 * @param {boolean} [wait=false] Whether to wait for the skill to be available
 	 */
-	async useSkill(skillIndex, force = false, wait = false) {
+	async useSkill(skillIndex, force = false, wait = false)
+	{
 		const sIdx = String(skillIndex);
 		const fn = force ? window.swf.ForceUseSkill : window.swf.UseSkill;
-		if (wait) {
+		if (wait)
+		{
 			await this.bot.sleep(this.bot.flash.call(window.swf.SkillAvailable, sIdx));
 		}
 		this.bot.flash.call(fn, sIdx);
@@ -56,7 +65,8 @@ class Combat {
 	 * Whether the current player has a target.
 	 * @returns {boolean}
 	 */
-	hasTarget() {
+	hasTarget()
+	{
 		return this.bot.flash.call(window.swf.HasTarget);
 	}
 
@@ -64,7 +74,8 @@ class Combat {
 	 * Rests the current player.
 	 * @returns {Promise<void>}
 	 */
-	async rest() {
+	async rest()
+	{
 		await this.bot.waitUntil(() => this.bot.world.isActionAvailable(GameAction.Rest));
 		this.bot.flash.call(window.swf.Rest);
 	}
@@ -74,41 +85,84 @@ class Combat {
 	 * @param {string} name The name or monMapID of the monster.
 	 * @returns {Promise<void>}
 	 */
-	async kill(name) {
+	async kill(name)
+	{
 		await this.bot.waitUntil(() => this.bot.world.isMonsterAvailable(name), null, 3);
 
-		if (!this.bot.player.alive || !this.bot.auth.loggedIn) {
+		if (!this.bot.player.alive || !this.bot.auth.loggedIn)
+		{
 			return;
 		}
 
 		let dead = false;
-		this.#intervalID = setIntervalAsync(async () => {
-			if (!this.bot.world.isMonsterAvailable(name) && !this.hasTarget()) {
-				dead = true;
-				await clearIntervalAsync(this.#intervalID);
+		let pause = false;
+
+		this.#intervalID = setIntervalAsync(async () =>
+		{
+			if (!this.bot.auth.loggedIn)
+			{
+				await this.stop();
 			}
 
-			const _name = name.toLowerCase();
-			if (_name === "escherion" && this.bot.world.isMonsterAvailable("Staff of Inversion")) {
-				this.attack("Staff of Inversion");
-			} else if (_name === "vath" && this.bot.world.isMonsterAvailable("Stalagbite")) {
-				this.attack("Stalagbite");
-			} else {
-				this.attack(name);
+			if (!this.bot.player.alive)
+			{
+				pause = true;
 			}
+			else
+			{
+				pause = false;
 
-			if (this.hasTarget()) {
-				await this.useSkill(this.skillSet[this.#skillSetIdx++], false, false);
-
-				if (this.#skillSetIdx >= this.skillSet.length) {
-					this.#skillSetIdx = 0;
+				if (this.bot.player.isAfk)
+				{
+					await this.stop();
+					await this.bot.sleep(1000);
+					await this.kill(name);
 				}
-				await this.bot.sleep(this.skillDelay);
+			}
+
+			if (!this.bot.world.isMonsterAvailable(name) && !this.hasTarget())
+			{
+				dead = true;
+				await this.stop();
 			}
 		}, 0);
 
-		while (!dead) {
-			await this.bot.sleep(1000);
+		while (dead == false || !this.#stop)
+		{
+			await this.#mutex.acquire();
+
+			if (pause)
+			{
+				await this.bot.sleep(100);
+			}
+			else
+			{
+				const _name = name.toLowerCase();
+				if (_name === "escherion" && this.bot.world.isMonsterAvailable("Staff of Inversion"))
+				{
+					this.attack("Staff of Inversion");
+				} else if (_name === "vath" && this.bot.world.isMonsterAvailable("Stalagbite"))
+				{
+					this.attack("Stalagbite");
+				} else
+				{
+					this.attack(name);
+				}
+
+				if (this.hasTarget())
+				{
+					await this.useSkill(this.skillSet[this.#skillSetIdx++], false, false);
+
+					if (this.#skillSetIdx >= this.skillSet.length)
+					{
+						this.#skillSetIdx = 0;
+					}
+
+					await this.bot.sleep(this.skillDelay);
+				}
+			}
+
+			this.#mutex.release();
 		}
 
 		this.bot.flash.call(window.swf.CancelAutoAttack);
@@ -123,18 +177,31 @@ class Combat {
 	 * @param {boolean} [isTemp=false] Whether the item is temporary.
 	 * @returns {Promise<void>}
 	 */
-	async killForItem(name, itemResolvable, itemQuantity, isTemp = false) {
+	async killForItem(name, itemResolvable, itemQuantity, isTemp = false)
+	{
 		const getItem = () =>
 			this.bot[isTemp ? "tempInventory" : "inventory"].resolve(itemResolvable);
 
 		const getQuantity = () => getItem()?.quantity ?? 0;
 
-		while (getQuantity() < itemQuantity) {
+		while (getQuantity() < itemQuantity)
+		{
 			await this.kill(name);
 			await this.bot.sleep(500);
-			if (!isTemp) {
+			if (!isTemp)
+			{
 				await this.bot.drops.pickup(itemResolvable);
 			}
 		}
+	}
+
+	/**
+	 * Forcefully stops any combat commands from continuing (e.g killing a monster.)
+	 * @returns {Promise<void>}
+	 */
+	async stop()
+	{
+		this.#stop = true;
+		await clearIntervalAsync(this.#intervalID);
 	}
 }
