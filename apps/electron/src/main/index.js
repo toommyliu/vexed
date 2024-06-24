b45b75cd2f873fa84062e01c906cf67ce7575ee6
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-06-23 18:51:49 -0700

    refactor: dont bundle pepperflash plugin in build

--- apps/electron/src/main/index.js
+++ apps/electron/src/main/index.js
@@ -1,18 +1,21 @@
 const fs = require('fs-extra');
 const { join } = require('path');
-const { app } = require('electron');
+const { app, dialog } = require('electron');
 
 const { createManager, createGame } = require('./windows');
 
 require('./ipc');
 
+const ARTIX_LAUNCHER = '/Applications/Artix Game Launcher.app';
+const PPFLASH_PLUGIN = join(
+	ARTIX_LAUNCHER,
+	'Contents/Resources/plugins/PepperFlashPlayer.plugin',
+);
+
 function registerFlashPlugin() {
 	const flashTrust = require('nw-flash-trust');
-	// TODO: add checks for app.isPackaged
-	app.commandLine.appendSwitch(
-		'ppapi-flash-path',
-		join(__dirname, '../../vendor/PepperFlashPlayer.plugin'),
-	);
+
+	app.commandLine.appendSwitch('ppapi-flash-path', PPFLASH_PLUGIN);
 
 	const flashPath = join(
 		app.getPath('userData'),
@@ -29,6 +32,26 @@ function registerFlashPlugin() {
 registerFlashPlugin();
 
 app.once('ready', async () => {
+	const launcher = await fs
+		.pathExists(ARTIX_LAUNCHER)
+		.then((ret) => ret)
+		.catch(() => false);
+
+	if (!launcher) {
+		dialog.showErrorBox('Error', 'Artix Game Launcher must be installed in the Applications Folder to make use of its Flash Player plugin.');
+		process.exit(1);
+	}
+
+	const ppflash = await fs
+		.pathExists(PPFLASH_PLUGIN)
+		.then((ret) => ret)
+		.catch(() => false);
+
+	if (!ppflash) {
+		dialog.showErrorBox('Error', 'Artix Game Launcher was found but no PepperFlashPlugin.plugin was found.');
+		process.exit(1);
+	}
+
 	const base = join(app.getPath('documents'), 'Vexed');
 	const preferencesPath = join(base, 'preferences.json');
 
