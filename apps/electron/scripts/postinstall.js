f3858e5fe907c89616959297453aeabbdba917a6
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2024-06-26 20:50:38 -0700

    refactor: improve postinstall script

--- apps/electron/scripts/postinstall.js
+++ apps/electron/scripts/postinstall.js
@@ -1,53 +1,57 @@
 // https://github.com/electron/electron/blob/11-x-y/npm/install.js
 
-console.log("Downloading Electron 11.5.0 Intel binary");
+console.log('Downloading Electron 11.5.0 Intel binary');
 
-const fs = require("node:fs");
-const path = require("node:path");
-const extract = require("extract-zip");
-const { downloadArtifact } = require("@electron/get");
-const { promisify } = require("node:util");
+const fs = require('fs-extra');
+const { join } = require('node:path');
+const { exec } = require('child_process');
+const { downloadArtifact } = require('@electron/get');
+const { promisify } = require('node:util');
 
 (async () => {
-	const nodeModules = path.join(__dirname, "../../../node_modules");
-	const electron = path.join(nodeModules, "electron");
-	const electronDist = path.join(electron, "dist");
+    const nodeModules = join(__dirname, '../../../node_modules');
+    const electron = join(nodeModules, 'electron');
+    const electronDist = join(electron, 'dist');
+    const version = join(electronDist, '_version.txt');
 
-	const exists = await promisify(fs.exists)(
-		path.join(electronDist, "_version.txt")
-	);
+    const exists = await fs.pathExists(version);
+    if (exists) {
+        console.log('Electron Intel binary already exists, skipping download');
+        return;
+    }
 
-	if (exists) {
-		console.log("Electron 11.5.0 already exists, skipping download");
-		return;
-	}
+    const zipPath = await downloadArtifact({
+        version: '11.5.0',
+        artifactName: 'electron',
+        force: process.env.force_no_cache === 'true',
+        cacheRoot: process.env.electron_config_cache,
+        platform: 'darwin',
+        arch: 'x64'
+    })
+        .then((res) => res)
+        .catch((err) => {
+            console.log('An error occured while downloading the Intel binary', err);
+            process.exit(1);
+        });
 
-	const zipPath = await downloadArtifact({
-		version: "11.5.0",
-		artifactName: "electron",
-		force: process.env.force_no_cache === "true",
-		cacheRoot: process.env.electron_config_cache,
-		platform: "darwin",
-		arch: "x64"
-	})
-		.then((res) => res)
-		.catch((err) => {
-			console.error(
-				"An error occured while downloading the Intel binary:",
-				err.stack
-			);
-			process.exit(1);
-		});
+    if (zipPath) {
+        try {
+            if (await fs.pathExists(electronDist)) {
+                await fs.rm(electronDist, { recursive: true });
+            }
 
-	if (zipPath) {
-		const writeFile = promisify(fs.writeFile);
+            exec(`unzip -q ${zipPath} -d ${electronDist}`, (error) => {
+                if (error) {
+                    console.log('An error occurred while trying to unzip', error);
+                    process.exit(1);
+                }
 
-		try {
-			await fs.promises.rm(electronDist, { recursive: true });
-			await extract(zipPath, { dir: electronDist });
-			await writeFile(path.join(electronDist, "_version.txt"), "11.5.0");
-		} catch (error) {
-			console.log(error);
-		}
-	}
+                console.log('Unzipped successfully');
+            });
+
+            await fs.outputFile(version, '11.5.0');
+        } catch (error) {
+            console.log('An error occured during the unzip process', error);
+        }
+    }
 })();
