6652fe5bab331ceac201a74fb95a73ca52d7f8e2
toommyliu <86510873+toommyliu@users.noreply.github.com>
2024-07-31 18:54:18 -0700

    fix: uncomment packet logger

--- apps/electron/src/renderer/game/handlers/index.js
+++ apps/electron/src/renderer/game/handlers/index.js
@@ -50,14 +50,13 @@ async function packetFromServer([packet]) {
 
 window.packetFromServer = packetFromServer;
 window.packetFromClient = async ([packet]) => {
-	// TODO: fix
 	/**
 	 * @type {WindowProxy|null}
 	 */
-	// const wnd = window.windows.packets.logger;
-	// if (wnd && !wnd.closed) {
-	// 	wnd.postMessage({ event: 'logger:packet', args: packet });
-	// }
+	const wnd = window.windows.packets.logger;
+	if (wnd) {
+		wnd.postMessage({ event: 'logger:packet', args: packet });
+	}
 };
 
 window.connection = ([state]) => {
