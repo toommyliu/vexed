cbeef335a044e19dffb4e1951f864a1ef0a1e650
toommyliu <86510873+toommyliu@users.noreply.github.com>
2024-08-19 20:40:07 -0700

    feat: add account option in tray

--- apps/electron/src/main/tray.js
+++ apps/electron/src/main/tray.js
@@ -3,10 +3,12 @@ const { app } = require('electron');
 const { join } = require('path');
 const fs = require('fs-extra');
 const fetch = require('node-fetch');
+const prompt = require('electron-prompt');
 
 const { createGame } = require('./windows');
 
 const ROOT = join(app.getPath('documents'), 'Vexed');
+const accountsPath = join(ROOT, 'accounts.json');
 
 let tray;
 let server;
@@ -16,21 +18,17 @@ app.on('ready', async () => {
 		nativeImage.createFromPath(join(__dirname, '../../build/logo.png')),
 	);
 
-	const exists = await fs
-		.pathExists(join(ROOT, 'accounts.json'))
-		.catch(() => {
-			dialog.showErrorBox('Failed to read accounts (1).');
-			app.quit();
-			return false;
-		});
+	const exists = await fs.pathExists(accountsPath).catch(() => {
+		dialog.showErrorBox('Failed to read accounts (1).');
+		app.quit();
+		return false;
+	});
 
 	if (exists) {
-		const accounts = await fs
-			.readJSON(join(ROOT, 'accounts.json'))
-			.catch(() => {
-				dialog.showErrorBox('Failed to read accounts (2).');
-				return [];
-			});
+		const accounts = await fs.readJSON(accountsPath).catch(() => {
+			dialog.showErrorBox('Failed to read accounts (2).');
+			return [];
+		});
 
 		const menu = [
 			{ label: 'Launch New Window', click: createGame },
@@ -48,6 +46,55 @@ app.on('ready', async () => {
 				},
 			},
 			{ type: 'separator' },
+			{
+				label: 'Add Account',
+				click: async () => {
+					const username = await prompt({
+						title: 'Add Account',
+						label: 'Username',
+						value: '',
+						inputAttrs: {
+							type: 'text',
+						},
+						type: 'input',
+					}).catch(() => null);
+
+					if (!username) {
+						return;
+					}
+
+					const password = await prompt({
+						title: 'Add Account',
+						label: 'Password',
+						value: '',
+						inputAttrs: {
+							type: 'password',
+						},
+						type: 'input',
+					}).catch(() => null);
+
+					if (!password) {
+						return;
+					}
+
+					const account = {
+						username,
+						password,
+					};
+
+					await fs
+						.writeJSON(
+							accountsPath,
+							[...(await fs.readJSON(accountsPath)), account],
+							{ spaces: 4 },
+						)
+						.catch(() => {
+							dialog.showErrorBox('Failed to write accounts.');
+							app.quit();
+						});
+				},
+			},
+			{ type: 'separator' },
 		];
 
 		await fetch('https://game.aq.com/game/api/data/servers')
@@ -56,12 +103,12 @@ app.on('ready', async () => {
 				menu.push({
 					label: 'Login server',
 					submenu: json.map((srv) => {
-						return {
-							label: srv.sName,
-							type: 'radio',
-							click: () => (server = srv.sName),
-						};
-					}),
+							return {
+								label: srv.sName,
+								type: 'radio',
+								click: () => (server = srv.sName),
+							};
+						}),
 				});
 			})
 			.catch(() => {
