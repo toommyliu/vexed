a855d124f4346bfa636295bcf9db5eb61d692f7a
toommyliu <86510873+toommyliu@users.noreply.github.com>
2024-09-07 10:09:25 -0700

    chore: setup eslint

---
+++ apps/electron/eslint.config.js
@@ -0,0 +1,48 @@
+const common = require('eslint-config-neon/common');
+const node = require('eslint-config-neon/node');
+const prettier = require('eslint-config-neon/prettier');
+const typescript = require('eslint-config-neon/typescript');
+const browser = require('eslint-config-neon/browser');
+
+const merge = require('lodash.merge');
+
+const commonFiles = '.ts';
+
+const commonRuleset = merge(...common, { files: [`**/*${commonFiles}`] });
+
+const nodeRuleset = merge(...node, { files: [`**/main/${commonFiles}`] });
+
+const browserRuleset = merge(...browser, {
+	files: [`**/renderer/${commonFiles}`],
+});
+
+const typeScriptRuleset = merge(...typescript, {
+	files: [`**/*${commonFiles}`],
+	languageOptions: {
+		parserOptions: {
+			project: ['tsconfig.eslint.json'],
+		},
+	},
+	rules: {
+		'@typescript-eslint/quotes': ['error', 'single'],
+	},
+});
+
+const prettierRuleset = merge(...prettier, { files: [`**/*${commonFiles}`] });
+
+/** @type {import('eslint').Linter.Config[]} */
+const rules = [
+	{
+		ignores: ['**/node_modules/', '.git/', '**/dist/', '**/build/'],
+	},
+	commonRuleset,
+	nodeRuleset,
+	browserRuleset,
+	typeScriptRuleset,
+	{
+		files: ['**/*.ts'],
+	},
+	prettierRuleset,
+];
+
+module.exports = rules;
