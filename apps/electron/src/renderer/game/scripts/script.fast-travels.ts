35f1fad5b86876f9e33bc67d2a7470590467836d
toommyliu <86510873+toommyliu@users.noreply.github.com>
2024-12-24 16:48:54 -0800

    feat: update fast travels interface

--- apps/electron/src/renderer/game/scripts/script.fast-travels.ts
+++ apps/electron/src/renderer/game/scripts/script.fast-travels.ts
@@ -10,52 +10,52 @@ window.addEventListener('ready', async () => {
 	const locations =
 		(await ipcRenderer.invoke(IPC_EVENTS.READ_FAST_TRAVELS)) ?? [];
 
-	{
-		const input = document.querySelector(
-			'#room-number',
-		) as HTMLInputElement;
-		input.addEventListener('input', () => {
-			const val = Number.parseInt(input.value, 10);
+	const input = document.querySelector('#room-number') as HTMLInputElement;
 
-			if (Number.isNaN(val)) {
-				roomNumber = 100_000;
-			}
+	input.addEventListener('input', () => {
+		const val = Number.parseInt(input.value, 10);
 
-			if (val < 1) {
-				input.value = '1';
-			} else if (val > 100_000) {
-				input.value = '100000';
-			} else {
-				roomNumber = val;
-			}
-		});
-	}
+		if (Number.isNaN(val)) {
+			roomNumber = 100_000;
+			return;
+		}
+
+		if (val < 1) {
+			input.value = '1';
+			roomNumber = 1;
+		} else if (val > 100_000) {
+			input.value = '100000';
+			roomNumber = 100_000;
+		} else {
+			roomNumber = val;
+		}
+	});
 
 	for (const location of locations) {
-		// The map to join is strictly required
 		if (!location.map) {
 			continue;
 		}
 
 		const div = document.createElement('div');
-		div.className = 'col-12 col-md-6 col-xl-4';
 
 		{
 			const btn = document.createElement('button');
-			btn.classList.add('btn', 'btn-primary', 'w-100');
+			btn.classList.add('w3-button', 'w3-round-medium');
 			btn.textContent = location.name;
 
 			// eslint-disable-next-line @typescript-eslint/no-loop-func
 			btn.addEventListener('click', async () => {
 				window.msgPort?.postMessage({
 					event: IPC_EVENTS.FAST_TRAVEL,
-					args: { ...location, roomNumber },
+					args: {
+						...location,
+						roomNumber,
+					},
 				});
 			});
 
 			div.appendChild(btn);
+			container.appendChild(div);
 		}
-
-		container.appendChild(div);
 	}
 });
