f837e56278ae0cbbc8aa49710354ca55d28566be
toommyliu <86510873+toommyliu@users.noreply.github.com>
2024-12-25 20:38:11 -0800

    fix: properly handle removing previous event handler

--- apps/electron/src/renderer/game/ipc/ipc.logger.ts
+++ apps/electron/src/renderer/game/ipc/ipc.logger.ts
@@ -1,10 +1,8 @@
 import { IPC_EVENTS } from '../../../common/ipc-events';
 
-let on = false;
+let listener: ((packet: string) => void) | null = null;
 
 const fn = (ev: MessageEvent, packet: string) => {
-	if (!on) return;
-
 	(ev.target as MessagePort).postMessage({
 		event: IPC_EVENTS.PACKET_LOGGER_PACKET,
 		args: { packet },
@@ -15,9 +13,10 @@ export default async function handler(ev: MessageEvent) {
 	const { event } = ev.data;
 
 	if (event === IPC_EVENTS.PACKET_LOGGER_START) {
-		on = true;
-		bot.on('packetFromClient', (packet: string) => fn(ev, packet));
+		listener = (packet: string) => fn(ev, packet);
+		bot.on('packetFromClient', listener);
 	} else if (event === IPC_EVENTS.PACKET_LOGGER_STOP) {
-		on = false;
+		bot.off('packetFromClient', listener!);
+		listener = null;
 	}
 }
