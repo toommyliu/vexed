977c691539faffbdca384ae672c205e8e5409eca
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-03-06 14:34:11 -0800

    feat: custom commands - add args support

--- src/renderer/game/botting/index.ts
+++ src/renderer/game/botting/index.ts
@@ -1,3 +1,4 @@
+import { ArgsError } from './ArgsError';
 import { Command } from './command';
 import { combatCommands } from './commands/combat';
 import { conditionsCommands } from './commands/conditions';
@@ -17,18 +18,28 @@ export const cmd = {
   ...miscCommands,
   ...questCommands,
 
-  add_command(name: string, fn: () => Promise<void> | void) {
-    // TODO: better way to expose fields
+  add_command(
+    name: string,
+    cmdFactory: (CommandClass: typeof Command) => Command,
+  ) {
+    // don't allow built-ins to be overwritten or custom commands to be duplicated
+    if (name in this) {
+      throw new ArgsError(`command ${name} already exists`);
+    }
+
+    const command = cmdFactory(Command);
+
+    if (!(command instanceof Command)) {
+      throw new ArgsError('cmdFactory must return an instance of Command');
+    }
 
     // @ts-expect-error - dynamic property
-    this[name] = (...args: unknown[]) => {
-      const custom_cmd = new Command();
-      custom_cmd.execute = fn.bind(custom_cmd);
-      custom_cmd.toString = () => name;
-      window.context.addCommand(custom_cmd);
+    // eslint-disable-next-line func-names
+    this[name] = function (...args: unknown[]) {
+      const newCmd = Object.create(command);
+      newCmd.args = args;
+      window.context.addCommand(newCmd);
     };
-
-    // console.log(`Added command: ${name}`);
   },
 };
 
