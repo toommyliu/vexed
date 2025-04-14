67c23e4e798a4a72134028bc72583a477b5434e3
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-04-14 15:57:34 -0700

    fix: update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,27 +8,6 @@ module.exports = {
     './src/renderer/game/scripts/**/*.ts',
     './src/renderer/manager/**/*.ts',
   ],
-  theme: {
-    extend: {
-      colors: {
-        dark: {
-          bg: '#0f111a',
-          panel: '#1c1e2a',
-          input: '#141622',
-          border: '#2d3748',
-          highlight: '#9f7aea',
-          accent: '#7e22ce',
-          success: '#a3e635',
-          danger: '#ef4444',
-          warning: '#f59e0b',
-          text: {
-            primary: '#f8fafc',
-            secondary: '#cbd5e1',
-            muted: '#94a3b8',
-          },
-        },
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
