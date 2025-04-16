afaf9cde66272cba8a4bfdb320c6c9ecf4c25852
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-04-15 22:03:13 -0700

    fix: resolve type warning

--- src/renderer/game/scripts/script.loader-grabber.ts
+++ src/renderer/game/scripts/script.loader-grabber.ts
@@ -9,7 +9,7 @@ const iconExpanded = `<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" vi
   <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
 </svg>`;
 
-let lastData: { data: TreeInputData[]; type: string } | null = null;
+let lastData: { data: unknown; type: string } | null = null;
 
 function createTreeNode(data: TreeNode): HTMLDivElement {
   const node = document.createElement('div');
