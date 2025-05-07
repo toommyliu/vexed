873f1b4e919a210156c3f312f6c08b94318de072
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-05-06 21:26:38 -0700

    Improve buying logic

--- src/renderer/game/botting/commands/misc/CommandBuyScrollOfLifeSteal.ts
+++ src/renderer/game/botting/commands/misc/CommandBuyScrollOfLifeSteal.ts
@@ -7,19 +7,26 @@ export class CommandBuyScrollOfLifeSteal extends Command {
   public qty!: number;
 
   public override async execute() {
-    if (
-      this.bot.inventory.contains(ITEM, this.qty) ||
-      this.bot.inventory.contains(ITEM, MAX_QTY)
-    ) {
+    if (this.bot.inventory.contains(ITEM, MAX_QTY)) {
       return;
     }
 
-    const count = this.bot.inventory.get(ITEM)?.quantity ?? 0;
+    const currentQty = this.bot.inventory.get(ITEM)?.quantity ?? 0;
+    const count = Math.min(
+      this.qty -
+        currentQty /* how many of the item needed to reach the desired qty */,
+      MAX_QTY -
+        currentQty /* how many of the item before reaching the max qty */,
+    );
+
+    if (count <= 0) {
+      return;
+    }
 
     await this.bot.world.join("arcangrove", "Potion", "Right");
     await this.bot.shops.load("211");
 
-    await this.bot.shops.buyByName(ITEM, MAX_QTY - count);
+    await this.bot.shops.buyByName(ITEM, count);
   }
 
   public override toString() {
