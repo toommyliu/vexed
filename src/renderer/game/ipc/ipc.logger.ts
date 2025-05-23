44683256d0f4504e43dd2612ee5809cf2e267a9c
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2025-05-22 20:59:55 -0700

    feat(packet-logger): display all packet variants (#149)

--- src/renderer/game/ipc/ipc.logger.ts
+++ src/renderer/game/ipc/ipc.logger.ts
@@ -1,37 +1,56 @@
 import { WINDOW_IDS } from "../../../common/constants";
 import { ipcRenderer } from "../../../common/ipc";
 import { IPC_EVENTS } from "../../../common/ipc-events";
-import { Logger } from "../../../common/logger";
+// import { Logger } from "../../../common/logger";
 import { Bot } from "../lib/Bot";
 
 const bot = Bot.getInstance();
-const logger = Logger.get("IpcLogger");
+// const logger = Logger.get("IpcLogger");
 
 let on = false;
-let hasListener = false;
+let hasListeners = false;
 
-const fn = async (packet: string) => {
+const fnClient = async (packet: string) => {
   if (!on) return;
 
   await ipcRenderer.callMain(IPC_EVENTS.MSGBROKER, {
-    ipcEvent: IPC_EVENTS.PACKET_LOGGER_PACKET,
-    data: { packet },
+    ipcEvent: IPC_EVENTS.PACKET_LOGGER_PACKET_CLIENT,
+    data: { packet, type: "client" },
+    windowId: WINDOW_IDS.PACKETS_LOGGER,
+  });
+};
+
+const fnServer = async (packet: string) => {
+  if (!on) return;
+
+  await ipcRenderer.callMain(IPC_EVENTS.MSGBROKER, {
+    ipcEvent: IPC_EVENTS.PACKET_LOGGER_PACKET_SERVER,
+    data: { packet, type: "server" },
+    windowId: WINDOW_IDS.PACKETS_LOGGER,
+  });
+};
+
+const fnPext = async (packet: Record<string, unknown>) => {
+  if (!on) return;
+
+  await ipcRenderer.callMain(IPC_EVENTS.MSGBROKER, {
+    ipcEvent: IPC_EVENTS.PACKET_LOGGER_PACKET_PEXT,
+    data: { packet, type: "pext" },
     windowId: WINDOW_IDS.PACKETS_LOGGER,
   });
 };
 
 ipcRenderer.answerMain(IPC_EVENTS.PACKET_LOGGER_START, async () => {
-  logger.info("start packet logger");
-
   on = true;
 
-  if (hasListener) return;
+  if (hasListeners) return;
 
-  hasListener = true;
-  bot.on("packetFromClient", fn);
+  hasListeners = true;
+  bot.on("packetFromClient", fnClient);
+  bot.on("packetFromServer", fnServer);
+  bot.on("pext", fnPext);
 });
 
 ipcRenderer.answerMain(IPC_EVENTS.PACKET_LOGGER_STOP, async () => {
-  logger.info("stop packet logger");
   on = false;
 });
