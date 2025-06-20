98b3869a4ab7ac277eb14a17a776359a2fc3f754
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-06-20 15:02:33 -0700

    feat: clear caches Closes #171

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -1,10 +1,43 @@
+import { Menu, Tray, app, dialog, nativeImage, session } from "electron";
+import fs from "fs-extra";
 import { join } from "path";
 import process from "process";
-import { Menu, Tray, app, nativeImage } from "electron";
+import { totalist } from "totalist";
 import { BRAND } from "../shared/constants";
 import { createAccountManager, createGame } from "./windows";
 
 let tray: Tray | null = null;
+
+const deleteDirectory = async (dirPath: string) => {
+  if (!(await fs.pathExists(dirPath))) {
+    console.log(`Directory does not exist: ${dirPath}`);
+    return;
+  }
+
+  try {
+    const dirs = new Set<string>();
+    const files = new Set<string>();
+
+    await totalist(dirPath, (_name, absPath, stats) => {
+      if (stats.isFile()) files.add(absPath);
+      else if (stats.isDirectory()) dirs.add(absPath);
+    });
+
+    // Delete files first
+    for (const filePath of files) await fs.unlink(filePath);
+
+    // Sort the directories by depth (a.k.a deepest path first)
+    const sortedDirs = Array.from(dirs).sort(
+      (a, b) => b.split("/").length - a.split("/").length,
+    );
+
+    // Delete directories
+    for (const dir of sortedDirs) await fs.rmdir(dir);
+
+    // Finally, deleteh the root directory
+    await fs.rmdir(dirPath);
+  } catch {}
+};
 
 const contextMenu = Menu.buildFromTemplate([
   {
@@ -14,6 +47,37 @@ const contextMenu = Menu.buildFromTemplate([
   {
     label: "Open Game",
     click: () => void createGame(),
+  },
+  { type: "separator" },
+  {
+    label: "Clear App Cache",
+    click: () => {
+      session.defaultSession?.clearStorageData({
+        storages: ["cookies", "appcache"],
+      });
+    },
+  },
+  {
+    label: "Clear Flash Cache",
+    click: async () => {
+      const flashPath = join(
+        app.getPath("userData"),
+        "Pepper Data",
+        "Shockwave Flash",
+        "WritableRoot",
+      );
+
+      await deleteDirectory(flashPath);
+
+      // A restart is required because we'd have to re-register the flash plugin
+      // So having the user restart the app is the easiest way to handle this.
+
+      await dialog.showMessageBox({
+        message:
+          "Flash cache cleared successfully. An app restart is required.",
+        type: "info",
+      });
+    },
   },
   { type: "separator" },
   {
