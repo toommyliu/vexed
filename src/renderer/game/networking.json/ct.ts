eb69d672aeeacd05f6a898a94efb91f67a93421c
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-06-21 09:22:06 -0700

    fix: playerDeath not emitting

--- src/renderer/game/networking.json/ct.ts
+++ src/renderer/game/networking.json/ct.ts
@@ -1,28 +1,40 @@
 import type { Bot } from "../lib/Bot";
 
 export function ct(bot: Bot, packet: CtPacket) {
-  for (const anim of packet?.anims ?? []) {
-    if (
-      anim?.msg?.toLowerCase()?.includes("prepares a counter attack") &&
-      bot.settings.counterAttack
-    ) {
-      bot.combat.cancelTarget();
-      bot.combat.cancelAutoAttack();
-      bot.combat.pauseAttack = true;
-      break;
+  if (Array.isArray(packet?.anims)) {
+    for (const anim of packet?.anims ?? []) {
+      if (
+        anim?.msg?.toLowerCase()?.includes("prepares a counter attack") &&
+        bot.settings.counterAttack
+      ) {
+        bot.combat.cancelTarget();
+        bot.combat.cancelAutoAttack();
+        bot.combat.pauseAttack = true;
+        break;
+      }
     }
   }
 
-  for (const aura of packet?.a ?? []) {
-    if (
-      aura?.cmd === "aura--" &&
-      aura?.aura?.nam === "Counter Attack" &&
-      bot.settings.counterAttack
-    ) {
-      const monMapID = aura!.tInf!.split(":")[1];
-      bot.combat.attack(`id:${monMapID}`);
-      bot.combat.pauseAttack = false;
-      break;
+  if (typeof packet?.p === "object") {
+    for (const [playerName, data] of Object.entries(packet?.p ?? {})) {
+      if (data?.intState === 0 && data?.intHP === 0) {
+        bot.emit("playerDeath", playerName);
+      }
+    }
+  }
+
+  if (Array.isArray(packet?.a)) {
+    for (const aura of packet?.a ?? []) {
+      if (
+        aura?.cmd === "aura--" &&
+        aura?.aura?.nam === "Counter Attack" &&
+        bot.settings.counterAttack
+      ) {
+        const monMapID = aura!.tInf!.split(":")[1];
+        bot.combat.attack(`id:${monMapID}`);
+        bot.combat.pauseAttack = false;
+        break;
+      }
     }
   }
 }
@@ -38,4 +50,10 @@ type CtPacket = {
   anims?: {
     msg?: string;
   }[];
+  p?: {
+    [key: string]: {
+      intHP?: number;
+      intState?: number;
+    };
+  };
 };
