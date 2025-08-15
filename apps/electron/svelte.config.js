7c1ee3abbdecec043c7103e8678e9ccd3aad4fe2
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-08-15 07:43:31 -0700

    chore(electron): update svelte config

--- apps/electron/svelte.config.js
+++ apps/electron/svelte.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  compilerOptions: {
-    warningFilter: (warning) =>
-      !warning.filename?.includes("node_modules") &&
-      !warning.code.startsWith("a11y"),
-  },
-};
+module.exports = {};
