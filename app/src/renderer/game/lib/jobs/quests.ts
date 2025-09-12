953445fd8a9b4ec8dca4a64e5854e1e3c1b95d53
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2025-09-11 17:55:54 -0700

    refactor: remove index tracking in QuestsJob (#224)

--- app/src/renderer/game/lib/jobs/quests.ts
+++ app/src/renderer/game/lib/jobs/quests.ts
@@ -1,69 +1,36 @@
-import { Logger } from "@vexed/logger";
 import type { Bot } from "../Bot";
 import { Job } from "./Job";
 
 export class QuestsJob extends Job {
-  private index = 0;
-
-  private snapshot: number[] = [];
-
-  private isRestarting = false;
-
-  private logger = Logger.get("questJob", { precision: 3 });
-
   public constructor(private readonly bot: Bot) {
     super("quests", 2);
-
-    this.snapshot = Array.from(this.bot.environment.questIds);
-
-    this.bot.environment.on("questIdsChanged", () => {
-      this.restart();
-    });
-  }
-
-  public restart(): void {
-    this.snapshot = Array.from(this.bot.environment.questIds);
-    this.index = 0;
-    this.isRestarting = true;
   }
 
   public async execute(): Promise<void> {
-    if (this.snapshot.length === 0) return;
-    if (this.isRestarting) {
-      this.isRestarting = false;
-      return;
+    const questIds = this.bot.environment.questIds;
+    if (questIds.size === 0) return;
+
+    for (const questId of questIds) {
+      if (!this.bot.quests.get(questId)) {
+        void this.bot.quests.load(questId);
+      }
+
+      if (
+        this.bot.flash.call(() => swf.questsIsInProgress(questId)) &&
+        this.bot.flash.call(() => swf.questsCanCompleteQuest(questId))
+      ) {
+        const maxTurnIns = this.bot.flash.call<string>(
+          "world.maximumQuestTurnIns",
+          questId,
+        );
+        const numMaxTurnIns = Number(maxTurnIns);
+        await this.bot.quests.complete(questId, numMaxTurnIns);
+      }
+
+      if (!this.bot.flash.call(() => swf.questsIsInProgress(questId)))
+        await this.bot.quests.accept(questId);
+
+      await this.bot.sleep(100);
     }
-
-    const questId = this.snapshot[this.index] as number;
-
-    if (this.index === 0) await this.bot.sleep(1_000);
-
-    // this.logger.info(`quest id this tick: ${questId} (${this.index})`);
-
-    if (!this.bot.quests.get(questId)) {
-      this.logger.info(`load quest id ${questId}`);
-      void this.bot.quests.load(questId);
-    }
-
-    if (
-      this.bot.flash.call(() => swf.questsIsInProgress(questId)) &&
-      this.bot.flash.call(() => swf.questsCanCompleteQuest(questId))
-    ) {
-      const maxTurnIns = this.bot.flash.call<string>(
-        "world.maximumQuestTurnIns",
-        questId,
-      );
-      const numMaxTurnIns = Number(maxTurnIns);
-      // this.logger.info(`complete quest id ${questId} with ${maxTurnIns}`);
-      await this.bot.quests.complete(questId, numMaxTurnIns);
-    }
-
-    if (!this.bot.flash.call(() => swf.questsIsInProgress(questId))) {
-      // this.logger.info(`accept quest id ${questId}`);
-      await this.bot.quests.accept(questId);
-    }
-
-    this.index = (this.index + 1) % this.snapshot.length;
-    await this.bot.sleep(250);
   }
 }
