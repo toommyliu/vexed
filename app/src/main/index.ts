129f3c1e3c073cb4ab5d9ec1d6c04b25c205ce9c
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2025-09-18 19:53:29 -0700

    fix: include DEFAULT_SETTINGS in Config (#243)

--- app/src/main/index.ts
+++ app/src/main/index.ts
@@ -5,7 +5,7 @@ import process from "process";
 import Config from "@vexed/config";
 import { registerIpcMain } from "@vexed/tipc/main";
 import { app, shell } from "electron";
-import { BRAND, DOCUMENTS_PATH } from "../shared/constants";
+import { BRAND, DEFAULT_SETTINGS, DOCUMENTS_PATH } from "../shared/constants";
 import type { Settings } from "../shared/types";
 import { ASSET_PATH } from "./constants";
 import { router } from "./tipc";
@@ -56,6 +56,7 @@ async function handleAppLaunch(argv: string[] = process.argv) {
     const settings = new Config<Settings>({
       configName: "settings",
       cwd: DOCUMENTS_PATH,
+      defaults: DEFAULT_SETTINGS,
     });
     await settings.load();
 
@@ -72,13 +73,18 @@ async function handleAppLaunch(argv: string[] = process.argv) {
       }
     }
 
+    let launchMode = settings.get("launchMode", "game")?.toLowerCase();
+    if (launchMode !== "manager" && launchMode !== "game") {
+      launchMode = "game";
+    }
+
     if (
-      settings.get("launchMode")?.toLowerCase() === "manager" ||
+      launchMode === "manager" ||
       argv.some((arg) => arg === "--manager" || arg === "-m")
     ) {
       await createAccountManager();
     } else if (
-      settings.get("launchMode")?.toLowerCase() === "game" ||
+      launchMode === "game" ||
       argv.some((arg) => arg === "--game" || arg === "-g")
     ) {
       const account = {
