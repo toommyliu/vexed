4e28564d1a3ea4da4f89fc40bfc89694946f570f
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2025-09-19 21:40:16 -0700

    fix: update asset path base (#249)

--- app/src/main/constants.ts
+++ app/src/main/constants.ts
@@ -1,12 +1,18 @@
-import { join } from "path";
+import { join, dirname } from "path";
+import process from "process";
 import { app } from "electron";
+
+export const IS_WINDOWS = process.platform === "win32";
+export const IS_MAC = process.platform === "darwin";
 
 export const IS_PACKAGED = app.isPackaged;
 
 export const ASSET_PATH = IS_PACKAGED
-  ? join(app.getAppPath(), "assets")
+  ? IS_WINDOWS
+    ? join(dirname(app.getPath("exe")), "assets")
+    : join(app.getAppPath(), "assets")
   : join(__dirname, "../../../assets");
 
 export const DIST_PATH = IS_PACKAGED
   ? join(app.getAppPath(), "dist")
-  : join(__dirname, "../../dist");
+  : join(process.cwd(), "dist");
