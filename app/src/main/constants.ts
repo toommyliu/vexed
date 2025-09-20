fe38d21fd98380d485c860753c47c7044b6f9c4e
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2025-09-19 21:32:09 -0700

    Revert "fix: asset path resolution for Windows (#247)"
    This reverts commit a14f1afb3df8f7ce09423c5d11a226ce57353601.

--- app/src/main/constants.ts
+++ app/src/main/constants.ts
@@ -1,16 +1,12 @@
-import { join, dirname } from "path";
-import process from "process";
+import { join } from "path";
 import { app } from "electron";
-
-export const IS_WINDOWS = process.platform === "win32";
-export const IS_MAC = process.platform === "darwin";
 
 export const IS_PACKAGED = app.isPackaged;
 
 export const ASSET_PATH = IS_PACKAGED
-    ? (IS_WINDOWS ? join(dirname(app.getPath("exe")), "assets") : join(app.getAppPath(), "assets"))
-    : join(process.cwd(), "assets");
+  ? join(app.getAppPath(), "assets")
+  : join(__dirname, "../../../assets");
 
 export const DIST_PATH = IS_PACKAGED
-    ? join(app.getAppPath(), "dist")
-    : join(process.cwd(), "dist");
+  ? join(app.getAppPath(), "dist")
+  : join(__dirname, "../../dist");
