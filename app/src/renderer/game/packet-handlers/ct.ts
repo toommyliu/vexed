f54f776f91ad2f6a4a8f775ffc8c6b439072a961
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-09-27 09:16:37 -0700

    Fix identation causing auras not to be handled

--- app/src/renderer/game/packet-handlers/ct.ts
+++ app/src/renderer/game/packet-handlers/ct.ts
@@ -31,100 +31,102 @@ export function ct(bot: Bot, packet: CtPacket) {
         break;
       }
     }
+  }
 
-    // player
-    if (typeof packet?.p === "object") {
-      for (const playerName in packet.p) {
-        if (!Object.hasOwn(packet.p, playerName)) continue;
-        const data = packet.p[playerName];
-        if (data?.intState === 0 && data?.intHP === 0) {
-          bot.emit("playerDeath", playerName);
-          AuraStore.playerAuras.delete(playerName);
-        }
+  // player
+  if (typeof packet?.p === "object") {
+    for (const playerName in packet.p) {
+      if (!Object.hasOwn(packet.p, playerName)) continue;
+      const data = packet.p[playerName];
+      if (data?.intState === 0 && data?.intHP === 0) {
+        bot.emit("playerDeath", playerName);
+        AuraStore.playerAuras.delete(playerName);
       }
     }
+  }
 
-    // auras
-    if (Array.isArray(packet?.a)) {
-      for (const aura of packet?.a ?? []) {
-        if (!aura?.cmd) continue;
+  // auras
+  if (Array.isArray(packet?.a)) {
+    for (const aura of packet?.a ?? []) {
+      if (!aura?.cmd) continue;
 
-        const parts = aura?.tInf?.split(":");
-        const type = parts?.[0] as "m" | "p" | undefined;
-        const tgtId = parts?.[1] as string | undefined;
+      const parts = aura?.tInf?.split(":");
+      const type = parts?.[0] as "m" | "p" | undefined;
+      const tgtId = parts?.[1] as string | undefined;
 
-        if (!type || !tgtId) continue;
+      if (!type || !tgtId) continue;
 
-        if (ADD_AURAS.has(aura.cmd)) {
-          if (type === "m") {
-            for (const aura_ of aura?.auras ?? []) {
-              const data: Aura = {
-                name: aura_.nam,
-                duration: aura_.dur ?? 0,
-                isNew: aura_?.isNew || false,
-              };
+      if (ADD_AURAS.has(aura.cmd)) {
+        if (type === "m") {
+          for (const aura_ of aura?.auras ?? []) {
+            const data: Aura = {
+              name: aura_.nam,
+              duration: aura_.dur ?? 0,
+              isNew: aura_?.isNew || false,
+            };
 
-              if ("val" in aura_) {
-                if (typeof aura_.val === "number") data.value = aura_.val;
-                else {
-                  console.log(
-                    "(1) unexpected aura val type:",
-                    typeof aura_.val,
-                    aura_.val,
-                  );
-                }
-
-                if (data.isNew) AuraStore.addMonsterAura(tgtId, data);
-                else AuraStore.refreshMonsterAura(tgtId, data);
+            if ("val" in aura_) {
+              if (typeof aura_.val === "number") data.value = aura_.val;
+              else {
+                console.log(
+                  "(1) unexpected aura val type:",
+                  typeof aura_.val,
+                  aura_.val,
+                );
               }
-            }
-          } else if (type === "p") {
-            const username = [...bot.world.playerUids].find(
-              ([, uid]) => uid === Number(tgtId),
-            )?.[0];
-            if (!username) continue;
 
-            for (const aura_ of aura?.auras ?? []) {
-              const data: Aura = {
-                name: aura_.nam,
-                duration: aura_.dur ?? 0,
-                isNew: aura_?.isNew || false,
-              };
-
-              if ("val" in aura_) {
-                if (typeof aura_.val === "number") data.value = aura_.val;
-                else {
-                  console.log(
-                    "(2) unexpected aura val type:",
-                    typeof aura_.val,
-                    aura_.val,
-                  );
-                }
-
-                if (data.isNew) AuraStore.addPlayerAura(username, data);
-                else AuraStore.refreshPlayerAura(username, data);
-              }
+              if (data.isNew) AuraStore.addMonsterAura(tgtId, data);
+              else AuraStore.refreshMonsterAura(tgtId, data);
             }
           }
-        } else if (REMOVE_AURAS.has(aura.cmd)) {
-          const auraName = aura?.aura?.nam;
-          if (!auraName) continue;
+        } else if (type === "p") {
+          const username = [...bot.world.playerUids].find(
+            ([, uid]) => uid === Number(tgtId),
+          )?.[0];
+          if (!username) continue;
 
-          if (type === "m") {
-            AuraStore.removeMonsterAura(tgtId, auraName);
+          for (const aura_ of aura?.auras ?? []) {
+            const data: Aura = {
+              name: aura_.nam,
+              duration: aura_.dur ?? 0,
+              isNew: aura_?.isNew || false,
+            };
 
-            if (auraName === "Counter Attack" && bot.settings.counterAttack) {
-              bot.combat.attack(`id:${tgtId}`);
-              bot.combat.pauseAttack = false;
+            if ("val" in aura_) {
+              if (typeof aura_.val === "number") data.value = aura_.val;
+              else {
+                console.log(
+                  "(2) unexpected aura val type:",
+                  typeof aura_.val,
+                  aura_.val,
+                  "aura obj:",
+                  aura_,
+                );
+              }
+
+              if (data.isNew) AuraStore.addPlayerAura(username, data);
+              else AuraStore.refreshPlayerAura(username, data);
             }
-          } else if (type === "p") {
-            const username = [...bot.world.playerUids].find(
-              ([, uid]) => uid === Number(tgtId),
-            )?.[0];
-            if (!username) continue;
-
-            AuraStore.removePlayerAura(username, auraName);
           }
+        }
+      } else if (REMOVE_AURAS.has(aura.cmd)) {
+        const auraName = aura?.aura?.nam;
+        if (!auraName) continue;
+
+        if (type === "m") {
+          AuraStore.removeMonsterAura(tgtId, auraName);
+
+          if (auraName === "Counter Attack" && bot.settings.counterAttack) {
+            bot.combat.attack(`id:${tgtId}`);
+            bot.combat.pauseAttack = false;
+          }
+        } else if (type === "p") {
+          const username = [...bot.world.playerUids].find(
+            ([, uid]) => uid === Number(tgtId),
+          )?.[0];
+          if (!username) continue;
+
+          AuraStore.removePlayerAura(username, auraName);
         }
       }
     }
