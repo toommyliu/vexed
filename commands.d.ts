1bbddff42a336ff96a2c7e9b1adb3053c7397ed0
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-09-27 09:59:41 -0700

    Docs

--- commands.d.ts
+++ commands.d.ts
@@ -303,7 +303,7 @@ If results are ambiguous, the first matching item will be used. Use itemType to 
   disable_provokecell(): void;
   /** Disables the "Skip cutscenes" setting. */
   disable_skipcutscenes(): void;
-  /** Performs loop taunt based on one or more strategies. See [looptaunt guide](/guides/looptaunt) for details. */
+  /** Performs loop taunt based on one or more strategies. See [looptaunt guide](/guides/looptaunt) for more details. */
   do_looptaunt(strategies: [string, number, number, string?][]): void;
   /** Enables the anti-counter attack setting. */
   enable_anticounter(): void;
