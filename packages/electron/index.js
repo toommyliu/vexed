b10d6a638d39cfde66f43e9c893aeaa18880aafa
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-09-30 16:38:04 -0700

    .

--- packages/electron/index.js
+++ packages/electron/index.js
@@ -5,14 +5,12 @@ var pathFile = path.join(__dirname, 'path.txt');
 console.log('expected pathFile at', pathFile);
 function getElectronPath() {
   if (fs.existsSync(pathFile)) {
-    console.log('found pathFile');
     var executablePath = fs.readFileSync(pathFile, 'utf-8');
     if (process.env.ELECTRON_OVERRIDE_DIST_PATH) {
       return path.join(process.env.ELECTRON_OVERRIDE_DIST_PATH, executablePath);
     }
     return path.join(__dirname, 'dist', executablePath);
   } else {
-    console.log('missing pathFile');
     throw new Error(
       'Electron failed to install correctly, please delete node_modules/electron and try installing again'
     );
