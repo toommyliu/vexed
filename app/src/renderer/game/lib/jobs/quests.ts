03fc0bf0ac8900f3ea93eb38c8454d73c15d3487
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-10-30 12:15:26 -0700

    fix(jobs/quests): fix double loading of quest

--- app/src/renderer/game/lib/jobs/quests.ts
+++ app/src/renderer/game/lib/jobs/quests.ts
@@ -1,8 +1,11 @@
+import { Mutex } from "async-mutex";
 import { client } from "@shared/tipc";
 import type { Bot } from "../Bot";
 import { Job } from "./Job";
 
 export class QuestsJob extends Job {
+  #mutex = new Mutex();
+
   public constructor(private readonly bot: Bot) {
     super("quests", 2);
   }
@@ -11,77 +14,75 @@ export class QuestsJob extends Job {
     const questIds = this.bot.environment.questIds;
     if (questIds.size === 0) return;
 
-    for (const questId of questIds) {
-      if (!this.bot.quests.get(questId)) {
-        void this.bot.quests.load(questId);
-      }
+    await this.#mutex.runExclusive(async () => {
+      for (const questId of questIds) {
+        if (!this.bot.quests.get(questId)) {
+          await this.bot.quests.load(questId);
+        }
 
-      if (this.bot.environment.autoRegisterRequirements) {
-        const quest = this.bot.quests.get(questId);
-        if (quest) {
-          const items = quest.requirements.reduce<string[]>(
-            (acc, req) => {
+        if (this.bot.environment.autoRegisterRequirements) {
+          const quest = this.bot.quests.get(questId);
+          if (quest) {
+            const items = quest.requirements.reduce<string[]>((acc, req) => {
               acc.push(req.itemName);
               return acc;
-            },
-            [],
-          );
+            }, []);
 
-          for (const itemName of items) {
-            this.bot.environment.addItemName(itemName);
-            void client.environment.updateState({
-              questIds: Array.from(this.bot.environment.questIds),
-              itemNames: Array.from(this.bot.environment.itemNames),
-              boosts: Array.from(this.bot.environment.boosts),
-              rejectElse: this.bot.environment.rejectElse,
-              autoRegisterRequirements: this.bot.environment.autoRegisterRequirements,
-              autoRegisterRewards: this.bot.environment.autoRegisterRewards,
-            });
+            for (const itemName of items) {
+              this.bot.environment.addItemName(itemName);
+              void client.environment.updateState({
+                questIds: Array.from(this.bot.environment.questIds),
+                itemNames: Array.from(this.bot.environment.itemNames),
+                boosts: Array.from(this.bot.environment.boosts),
+                rejectElse: this.bot.environment.rejectElse,
+                autoRegisterRequirements:
+                  this.bot.environment.autoRegisterRequirements,
+                autoRegisterRewards: this.bot.environment.autoRegisterRewards,
+              });
+            }
           }
         }
-      }
 
-      if (this.bot.environment.autoRegisterRewards) {
-        const quest = this.bot.quests.get(questId);
-        if (quest) {
-          const items = quest.rewards.reduce<string[]>(
-            (acc, reward) => {
+        if (this.bot.environment.autoRegisterRewards) {
+          const quest = this.bot.quests.get(questId);
+          if (quest) {
+            const items = quest.rewards.reduce<string[]>((acc, reward) => {
               acc.push(reward.itemName);
               return acc;
-            },
-            [],
-          );
+            }, []);
 
-          for (const itemName of items) {
-            this.bot.environment.addItemName(itemName);
-            void client.environment.updateState({
-              questIds: Array.from(this.bot.environment.questIds),
-              itemNames: Array.from(this.bot.environment.itemNames),
-              boosts: Array.from(this.bot.environment.boosts),
-              rejectElse: this.bot.environment.rejectElse,
-              autoRegisterRequirements: this.bot.environment.autoRegisterRequirements,
-              autoRegisterRewards: this.bot.environment.autoRegisterRewards,
-            });
+            for (const itemName of items) {
+              this.bot.environment.addItemName(itemName);
+              void client.environment.updateState({
+                questIds: Array.from(this.bot.environment.questIds),
+                itemNames: Array.from(this.bot.environment.itemNames),
+                boosts: Array.from(this.bot.environment.boosts),
+                rejectElse: this.bot.environment.rejectElse,
+                autoRegisterRequirements:
+                  this.bot.environment.autoRegisterRequirements,
+                autoRegisterRewards: this.bot.environment.autoRegisterRewards,
+              });
+            }
           }
         }
+
+        if (
+          this.bot.flash.call(() => swf.questsIsInProgress(questId)) &&
+          this.bot.flash.call(() => swf.questsCanCompleteQuest(questId))
+        ) {
+          const maxTurnIns = this.bot.flash.call<string>(
+            "world.maximumQuestTurnIns",
+            questId,
+          );
+          const numMaxTurnIns = Number(maxTurnIns);
+          await this.bot.quests.complete(questId, numMaxTurnIns);
+        }
+
+        if (!this.bot.flash.call(() => swf.questsIsInProgress(questId)))
+          await this.bot.quests.accept(questId);
+
+        await this.bot.sleep(100);
       }
-
-      if (
-        this.bot.flash.call(() => swf.questsIsInProgress(questId)) &&
-        this.bot.flash.call(() => swf.questsCanCompleteQuest(questId))
-      ) {
-        const maxTurnIns = this.bot.flash.call<string>(
-          "world.maximumQuestTurnIns",
-          questId,
-        );
-        const numMaxTurnIns = Number(maxTurnIns);
-        await this.bot.quests.complete(questId, numMaxTurnIns);
-      }
-
-      if (!this.bot.flash.call(() => swf.questsIsInProgress(questId)))
-        await this.bot.quests.accept(questId);
-
-      await this.bot.sleep(100);
-    }
+    });
   }
 }
