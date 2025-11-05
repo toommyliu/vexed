48de3cee7f8602f90bc49bc3193b62c87043deca
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-04 21:08:28 -0800

    feat: Dialog and ConfirmDialog

--- packages/ui/src/lib/index.js
+++ packages/ui/src/lib/index.js
@@ -1,3 +1,6 @@
 export { default as Button } from "./Button.svelte";
+export { default as Dialog } from "./Dialog.svelte";
+export { default as ConfirmDialog } from "./ConfirmDialog.svelte";
 export { default as VirtualList } from "./vlist/VirtualList.svelte";
+
 export { cn } from "./util/cn";
