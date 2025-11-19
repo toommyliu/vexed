5795ece02885259cbdb47f4e7dd93f0e22d8120b
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-19 05:23:40 -0800

    update

--- app/src/renderer/packets/spammer/main.ts
+++ app/src/renderer/packets/spammer/main.ts
@@ -1,4 +1,7 @@
-import { mount } from "svelte";
-import App from "./app.svelte";
+import App from './App.svelte';
 
-export default mount(App, { target: document.body });
+const app = new App({
+    target: document.body,
+});
+
+export default app;
