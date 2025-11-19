acbe1eebc35aaeba54a2e695fe7cafdf97ce3298
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-19 06:02:12 -0800

    improve root check

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,8 +1,8 @@
-# !/bin/bash
+#!/bin/bash
 
-if [ "$(pwd)" != "$(cd .. && pwd)" ]; then
+if [ ! -f "package.json" ]; then
   echo "Please execute this script from the root directory"
-  echo "currently in: $(pwd)"
+  echo "Currently in: $(pwd)"
   exit 1
 fi
 
@@ -12,6 +12,7 @@ find . -name "dist" -type d -prune -exec printf 'Deleting: %s\n' '{}' \; -exec r
 find . -name "build" -type d -prune -exec printf 'Deleting: %s\n' '{}' \; -exec rm -rf '{}' +
 
 read -p "Reinstall dependencies? [y/N] " -n 1 -r
+echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   pnpm install
 fi
