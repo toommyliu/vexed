d236d0c69aeba5de0d10610663eed87e848a88f0
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-19 06:09:54 -0800

    .

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -11,8 +11,13 @@ find . -name "pnpm-lock.yaml" -type f -exec printf 'Deleting: %s\n' '{}' \; -exe
 find . -name "dist" -type d -prune -exec printf 'Deleting: %s\n' '{}' \; -exec rm -rf '{}' +
 find . -name "build" -type d -prune -exec printf 'Deleting: %s\n' '{}' \; -exec rm -rf '{}' +
 
+# svelte stuff
+find . -name ".svelte-kit" -type d -prune -exec printf 'Deleting: %s\n' '{}' \; -exec rm -rf '{}' +
+
+# packages/electron specifics
+find . -name "path.txt" -type f -exec printf 'Deleting: %s\n' '{}' \; -exec rm -f '{}' \;
+
 read -p "Reinstall dependencies? [y/N] " -n 1 -r
-echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
   pnpm install
 fi
