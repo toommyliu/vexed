05a048a29d412a05ae9806997bd21c3e5d647958
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-20 11:23:52 -0800

    fix: add Select barrel

--- packages/ui/src/lib/index.js
+++ packages/ui/src/lib/index.js
@@ -8,7 +8,16 @@ export { default as VirtualList } from "./vlist/VirtualList.svelte";
 // Form components
 export { default as Input } from "./components/Input.svelte";
 export { default as Checkbox } from "./components/Checkbox.svelte";
-export { default as Select } from "./components/Select.svelte";
+export {
+    Select,
+    SelectTrigger,
+    SelectContent,
+    SelectItem,
+    SelectValue,
+    SelectGroup,
+    SelectGroupLabel,
+    SelectSeparator,
+} from "./components/select/index.js";
 
 // Layout components
 export {
