b9833e1b57833d1411f855af02490c53238320a1
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-20 11:40:52 -0800

    feat: Badge, Kbd, Label, Dialog

---
+++ packages/ui/src/lib/components/dialog/DialogHeader.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { cn } from "$lib/util/cn";
+
+    /** @type {string} */
+    let className = undefined;
+    export { className as class };
+</script>
+
+<div
+    class={cn(
+        "flex flex-col gap-1 px-6 pt-6 pb-4 text-center last:pb-6 sm:text-left",
+        className,
+    )}
+    data-slot="dialog-header"
+    {...$$restProps}
+>
+    <slot />
+</div>
---
+++ packages/ui/src/lib/components/dialog/index.js
@@ -0,0 +1,19 @@
+import Dialog from "./Dialog.svelte";
+import DialogTrigger from "./DialogTrigger.svelte";
+import DialogContent from "./DialogContent.svelte";
+import DialogHeader from "./DialogHeader.svelte";
+import DialogFooter from "./DialogFooter.svelte";
+import DialogTitle from "./DialogTitle.svelte";
+import DialogDescription from "./DialogDescription.svelte";
+import DialogClose from "./DialogClose.svelte";
+
+export {
+    Dialog,
+    DialogTrigger,
+    DialogContent,
+    DialogHeader,
+    DialogFooter,
+    DialogTitle,
+    DialogDescription,
+    DialogClose,
+};
