3d2bd120585e7e727d0b87ca86eb19670aa4a6c0
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-20 22:46:17 -0800

    fix: tree-shaked components

---
+++ packages/ui/src/lib/components.js
@@ -0,0 +1,23 @@
+export { default as Button } from "./Button.svelte";
+export { default as VirtualList } from "./vlist/VirtualList.svelte";
+
+export { default as Input } from "./components/Input.svelte";
+export { default as Checkbox } from "./components/Checkbox.svelte";
+export * as Select from "./components/select/index.js";
+export * as Combobox from "./components/combobox/index.js";
+export { default as Dropdown } from "./components/Dropdown.svelte";
+
+export { default as Badge } from "./components/Badge.svelte";
+export { default as Kbd } from "./components/Kbd.svelte";
+export { default as Label } from "./components/Label.svelte";
+export * as Card from "./components/card/index.js";
+
+export * as Dialog from "./components/dialog/index.js";
+
+export * as Group from "./components/group/index.js";
+export * as Empty from "./components/empty/index.js";
+export * as Tabs from "./components/tabs/index.js";
+export * as Menu from "./components/menu/index.js";
+export * as Alert from "./components/alert/index.js";
+export { Switch } from "./components/switch/index.js";
+export * as NumberField from "./components/number-field/index.js";
