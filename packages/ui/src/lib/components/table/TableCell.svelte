0edb0c739aad5b3178ce9199af685581ce610919
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-21 09:15:12 -0800

    feat: table component

---
+++ packages/ui/src/lib/components/table/TableCell.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { cn } from "$lib/util/cn";
+    import type { HTMLAttributes } from "svelte/elements";
+    import type { Snippet } from "svelte";
+
+    interface Props extends HTMLAttributes<HTMLTableCellElement> {
+        children?: Snippet;
+    }
+
+    let { class: className, children, ...restProps }: Props = $props();
+</script>
+
+<td
+    class={cn(
+        "whitespace-nowrap p-2 align-middle has-[[role=checkbox]]:pe-0 [&>[role=checkbox]]:translate-y-[2px]",
+        className,
+    )}
+    data-slot="table-cell"
+    {...restProps}
+>
+    {@render children?.()}
+</td>
