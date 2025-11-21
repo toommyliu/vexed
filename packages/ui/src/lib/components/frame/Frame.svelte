bfe9cc8fb6ec6e4e88432f0e9693b878ed391d73
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-21 12:02:16 -0800

    feat: Frame component

---
+++ packages/ui/src/lib/components/frame/Frame.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { cn } from "$lib/util/cn";
+
+    let { children, class: className = undefined, ...props } = $props();
+</script>
+
+<div
+    class={cn("relative flex flex-col rounded-2xl bg-muted p-1", className)}
+    data-slot="frame"
+    {...props}
+>
+    {@render children?.()}
+</div>
---
+++ packages/ui/src/lib/components/frame/index.js
@@ -0,0 +1,6 @@
+export { default as Root } from "./Frame.svelte";
+export { default as Panel } from "./FramePanel.svelte";
+export { default as Header } from "./FrameHeader.svelte";
+export { default as Title } from "./FrameTitle.svelte";
+export { default as Description } from "./FrameDescription.svelte";
+export { default as Footer } from "./FrameFooter.svelte";
