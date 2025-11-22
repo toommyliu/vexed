90b009dd1b6f6146abacf0e523269a17cf09e95d
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-21 22:48:15 -0800

    feat: Separator, Textarea, Field, InputGroup

---
+++ packages/ui/src/lib/components/Textarea.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import { cn } from "$lib/util/cn";
+    import type { HTMLTextareaAttributes } from "svelte/elements";
+
+    interface Props extends HTMLTextareaAttributes {
+        size?: "sm" | "default" | "lg";
+        unstyled?: boolean;
+        ref?: HTMLTextAreaElement;
+    }
+
+    let {
+        class: className,
+        size = "default",
+        unstyled = false,
+        ref = $bindable(),
+        ...props
+    }: Props = $props();
+</script>
+
+<span
+    class={cn(
+        !unstyled &&
+            "relative inline-flex w-full rounded-lg border border-input bg-background bg-clip-padding text-base shadow-xs ring-ring/24 transition-shadow before:pointer-events-none before:absolute before:inset-0 before:rounded-[calc(var(--radius-lg)-1px)] has-focus-visible:has-aria-invalid:border-destructive/64 has-focus-visible:has-aria-invalid:ring-destructive/16 has-aria-invalid:border-destructive/36 has-focus-visible:border-ring has-disabled:opacity-64 has-[:disabled,:focus-visible,[aria-invalid]]:shadow-none has-focus-visible:ring-[3px] not-has-disabled:has-not-focus-visible:not-has-aria-invalid:before:shadow-[0_1px_--theme(--color-black/4%)] sm:text-sm dark:bg-input/32 dark:bg-clip-border dark:has-aria-invalid:ring-destructive/24 dark:not-has-disabled:has-not-focus-visible:not-has-aria-invalid:before:shadow-[0_-1px_--theme(--color-white/8%)]",
+        className,
+    ) || undefined}
+    data-size={size}
+    data-slot="textarea-control"
+>
+    <textarea
+        bind:this={ref}
+        class={cn(
+            "field-sizing-content min-h-17.5 w-full rounded-[inherit] bg-transparent px-[calc(theme(spacing.3)-1px)] py-[calc(theme(spacing[1.5])-1px)] outline-none max-sm:min-h-20.5",
+            size === "sm" &&
+                "min-h-16.5 px-[calc(theme(spacing[2.5])-1px)] py-[calc(theme(spacing.1)-1px)] max-sm:min-h-19.5",
+            size === "lg" &&
+                "min-h-18.5 py-[calc(theme(spacing.2)-1px)] max-sm:min-h-21.5",
+        )}
+        data-slot="textarea"
+        {...props}
+    ></textarea>
+</span>
