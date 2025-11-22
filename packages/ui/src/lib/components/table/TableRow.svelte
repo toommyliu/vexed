980488cd3c2f182adc9d500f162a3d822cbdde74
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-11-21 22:06:14 -0800

    rounding to tables

--- packages/ui/src/lib/components/table/TableRow.svelte
+++ packages/ui/src/lib/components/table/TableRow.svelte
@@ -12,7 +12,7 @@
 
 <tr
     class={cn(
-        "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted",
+        "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted rounded-lg",
         className,
     )}
     data-slot="table-row"
