ab894308efb7cd623e62daa41afcf53f895b370e
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-12-13 11:19:27 -0800

    fix

--- app/src/renderer/game/botting/commands/misc/CommandAutoRelogin.ts
+++ app/src/renderer/game/botting/commands/misc/CommandAutoRelogin.ts
@@ -10,7 +10,7 @@ export class CommandAutoRelogin extends Command {
 
   protected override _skipDelay = true;
 
-  public override executeImpl() {
+  public override async executeImpl() {
     const username = this.username ?? this.bot.auth.username;
     const password = this.password ?? this.bot.auth.password;
     const server =
