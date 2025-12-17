8cc92c3badf8ab2c3eef5cf1fe42cc841923ecb8
toommyliu <86510873+toommyliu@users.noreply.github.com>
2025-12-17 11:38:53 -0800

    ccqr

--- app/src/renderer/game/packet-handlers/json/ccqr.ts
+++ app/src/renderer/game/packet-handlers/json/ccqr.ts
@@ -1,7 +1,26 @@
+import logger from 'electron-log';
 import type { JsonPacketHandler } from "../types";
+
+const log = logger.scope('ccqr');
+
+type CcqrData = {
+  QuestID: number;
+  bSuccess: number;
+  cmd: 'ccqr';
+  rewardObj: {
+    iCP: number;
+    intCoins: number;
+    intExp: number;
+    intGold: number;
+    typ: string;
+  };
+  sName: string;
+};
 
 export default {
   cmd: "ccqr",
   type: "json",
-  run: () => {},
+  run: (_bot, data: CcqrData) => {
+    if (data.bSuccess === 0) log.debug(`Failed to complete quest: ${data.QuestID}`);
+  },
 } satisfies JsonPacketHandler<unknown>;
