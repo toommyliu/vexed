dc9da7403abc14dff4528edbe9ff22c79e3019dd
toommyliu <86510873+toommyliu@users.noreply.github.com>
2026-01-02 19:39:30 -0800

    refactor: port skill sets to json, add migration helper

--- app/src/renderer/game/app.svelte
+++ app/src/renderer/game/app.svelte
@@ -27,7 +27,7 @@
     IS_WINDOWS,
     IS_MAC,
   } from "~/shared/constants";
-  import { parseSkillString } from "./util/skillParser";
+  import { parseSkillSetJson, type SkillSetJson } from "./util/skillParser";
   import log from "electron-log";
 
   import CommandOverlay from "./components/CommandOverlay.svelte";
@@ -341,7 +341,7 @@
       const currentCls = bot.player.className;
 
       const skillSet =
-        appState.skillSets?.get(currentCls) ?? parseSkillString("1;2;3;4|150");
+        appState.skillSets?.get(currentCls) ?? parseSkillSetJson({ skills: [1, 2, 3, 4], delay: 150 });
       const skillList = skillSet.skills;
       let idx = 0;
 
@@ -416,8 +416,8 @@
     async () => {
       const skillSets = await client.game.getSkillSets();
 
-      for (const [className, skillList] of Object.entries(skillSets)) {
-        const res = parseSkillString(skillList);
+      for (const [className, skillSetJson] of Object.entries(skillSets)) {
+        const res = parseSkillSetJson(skillSetJson as SkillSetJson);
         if (res) appState.skillSets.set(className.toUpperCase(), res);
       }
 
