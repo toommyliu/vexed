fb176dc4673f932014cb9c4037b2ab1b5e07b3db
toommyliu <86510873+toommyliu@users.noreply.github.com>
2026-01-10 22:52:34 -0800

    feat(commands): enable/disable death ads

---
+++ app/src/renderer/game/botting/commands/misc/CommandSettingDisableDeathAds.ts
@@ -0,0 +1,15 @@
+import { Command } from "~/botting/command";
+
+export class CommandSettingDisableDeathAds extends Command {
+    public state: boolean = false;
+
+    protected override _skipDelay = true;
+
+    public override executeImpl() {
+        this.bot.settings.disableDeathAds = this.state;
+    }
+
+    public override toString() {
+        return `${this.state ? "Disable" : "Enable"} death ads`;
+    }
+}
