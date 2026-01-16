f59eb0f5f4b94ce518cd04659a5e679d5f13ed0a
toommyliu <86510873+toommyliu@users.noreply.github.com>
2026-01-15 21:50:21 -0800

    fix: safe iterate

--- app/src/renderer/game/packet-handlers/json/load-inventory-big.ts
+++ app/src/renderer/game/packet-handlers/json/load-inventory-big.ts
@@ -8,16 +8,20 @@ registerJsonHandler<LoadInventoryBigPacket>(
     bot.player.house.items.all().clear();
     bot.player.inventory.items.all().clear();
 
-    for (const faction of packet.factions) bot.player.factions.add(faction);
-    for (const item of packet.hitems) bot.player.house.items.add(item);
-    for (const item of packet.items) bot.player.inventory.items.add(item);
+    console.log("packet", packet);
 
+    for (const faction of packet.factions ?? [])
+      bot.player.factions.add(faction);
     console.log(
       `[loadBigInventory] :: ${bot.player.factions.all().size} factions`,
     );
+
+    for (const item of packet.hitems ?? []) bot.player.house.items.add(item);
     console.log(
       `[loadBigInventory] :: ${bot.player.house.items.all().size} house items`,
     );
+
+    for (const item of packet.items ?? []) bot.player.inventory.items.add(item);
     console.log(
       `[loadBigInventory] :: ${bot.player.inventory.items.all().size} items`,
     );
@@ -27,7 +31,7 @@ registerJsonHandler<LoadInventoryBigPacket>(
 type LoadInventoryBigPacket = {
   bankCount: number; // how many non-AC items are in the bank
   cmd: "loadInventoryBig";
-  factions: FactionData[];
-  hitems: ItemData[];
-  items: ItemData[];
+  factions?: FactionData[];
+  hitems?: ItemData[];
+  items?: ItemData[];
 };
