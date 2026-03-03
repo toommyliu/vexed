ef22518bbaa0764597ad38b473b3c67503d862b4
toommyliu <86510873+toommyliu@users.noreply.github.com>
2026-03-03 06:03:36 -0800

    fix(ui/select): show focus ring in content

--- packages/ui/src/lib/components/core/select/select-content.svelte
+++ packages/ui/src/lib/components/core/select/select-content.svelte
@@ -21,6 +21,7 @@
     class={cn(
       "relative z-50 max-h-60 min-w-[var(--reference-width)] max-w-[calc(100vw-1rem)] select-none overflow-y-auto overflow-x-hidden rounded-lg border border-border bg-popover p-1 scroll-py-1 text-popover-foreground shadow-lg outline-none",
       "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-97 data-[state=open]:zoom-in-98 duration-200 ease-out",
+      "focus-visible:border-ring focus-visible:ring-[1px]",
       className,
     )}
     {...restProps}
