20fe782d3db69656d48ed99c0196bae0b0e179c9
toommyliu <86510873+toommyliu@users.noreply.github.com>
2026-03-03 22:06:09 -0800

    fix: narrow type

--- app/src/main/util/browser-window.ts
+++ app/src/main/util/browser-window.ts
@@ -1,5 +1,5 @@
 import type { BrowserWindow } from "electron";
 
-export function isWindowUsable(window: BrowserWindow) {
-  return !window.isDestroyed() && !window.webContents.isDestroyed();
+export function isWindowUsable(window: BrowserWindow | null) {
+  return !window?.isDestroyed() && !window?.webContents.isDestroyed();
 }
