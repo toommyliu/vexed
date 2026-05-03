be9fe620a6ada551f243d15d14399e475fe8f6dc
toommyliu <86510873+toommyliu@users.noreply.github.com>
2026-05-02 21:28:37 -0700

    chore(ui): remove stale svelte package

--- packages/ui/src/lib/components/core/app-frame/index.ts
+++
@@ -1,4 +0,0 @@
-export type Orientation = "vertical" | "horizontal";
-export { default as Root } from "./app-frame.svelte";
-export { default as Header } from "./app-frame-header.svelte";
-export { default as Body } from "./app-frame-body.svelte";
--- packages/ui/src/lib/components/core/select/context.ts
+++
@@ -1,6 +0,0 @@
-export interface SelectContext<T> {
-  registerItem: (item: { value: T; label: string; disabled: boolean }) => void;
-  unregisterItem: (value: T) => void;
-  items?: { value: T; label: string; disabled: boolean }[];
-  value?: string[];
-}
