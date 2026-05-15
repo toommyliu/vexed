dd23511e7596701ad92f029b2bfa97082d85dfa1
Tommy Liu <86510873+toommyliu@users.noreply.github.com>
2026-05-15 15:21:28 -0700

    refactor: remove fs, config package (#373)

--- packages/fs/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./errors";
-export * from "./dir";
-export * from "./file";
-export * from "./json";
--- packages/fs/src/util.ts
+++
@@ -1,8 +0,0 @@
-export function isEnoentError(cause: unknown): boolean {
-  return (
-    !!cause &&
-    typeof cause === "object" &&
-    "code" in cause &&
-    (cause as { code?: unknown }).code === "ENOENT"
-  );
-}
